6ed1a63099c0eae49b2a1988d61ae309696ae895
Andrew Zhu <mommothazaz123@gmail.com>
2017-01-14 13:23:38 -0800

    forgot to setup templates

--- web/web.py
+++ web/web.py
@@ -22,6 +22,7 @@ class Web:
         self.app = web.Application(loop=self.loop)
         aiohttp_jinja2.setup(self.app,
                              loader=jinja2.FileSystemLoader('./web/templates'))
+        self.setup_middlewares(self.app)
         self.app.router.add_static('/5etools/', './web/data/5etools/')
         self.run_app(self.app, host=os.environ.get('HOST'), port=os.environ.get('PORT'))
         

78e5760ff273a413333011ae1a7f9d8ead4706f4
Andrew Zhu <mommothazaz123@gmail.com>
2017-03-27 16:12:49 -0700

    shards, take 4

--- launch.sh
+++ launch.sh
@@ -2,12 +2,12 @@
 # launch.sh
 # launch discord bots
 
-echo "Launching web"
-python3 webLauncher.py &
-
 for ((i=0; i<$1; i++))
 do
     echo "Launching shard $2 $i with command python3 dbot.py -s $i $2"
     python3 dbot.py -s $i $2 &
     sleep 10
 done
+
+echo "Launching web"
+python3 webLauncher.py

ac73e0e35f01583d6c64910d14bbf0e69bf378ea
Andrew Zhu <mommothazaz123@gmail.com>
2017-04-12 09:16:30 -0400

    added auto restart script to overseer process

--- beAnnoying.py
+++ beAnnoying.py
@@ -6,6 +6,7 @@ Created on Mar 27, 2017
 import copy
 import os
 import signal
+import subprocess
 import sys
 import time
 
@@ -16,18 +17,47 @@ from utils.dataIO import DataIO
 class Overseer():
     def __init__(self):
         self.db = DataIO(TESTING, credentials.test_database_url)
+        self.shards = {}
 
 TESTING = False
+RUNNING = True
 if "test" in sys.argv:
     TESTING = True
 bot = Overseer()
 
 def init():
     signal.signal(signal.SIGTERM, sigterm_handler)
+    launch_shards()
     clean_shard_servers()
 
 def loop():
-    time.sleep(1)
+    time.sleep(30)
+    if RUNNING:
+        check_shards()
+    
+    
+def launch_shards():
+    for shard in range(int(os.environ.get('SHARDS', 1))):
+        if TESTING:
+            print("Launching shard test {}".format(shard))
+            bot.shards[shard] = subprocess.Popen(['python3', 'dbot.py', '-s', str(shard), 'test'])
+        else:
+            print("Launching shard production {}".format(shard))
+            bot.shards[shard] = subprocess.Popen(['python3', 'dbot.py', '-s', str(shard)])
+        time.sleep(10)
+    print("Shards launched: {}".format({shard: process.pid for shard, process in bot.shards.items()}))
+    
+def check_shards():
+    for shard, process in bot.shards.items():
+        if process.poll() is not None:
+            print('Shard {} crashed with exit code {}, restarting...'.format(shard, process.returncode))
+            if TESTING:
+                print("Launching shard test {}".format(shard))
+                bot.shards[shard] = subprocess.Popen(['python3', 'dbot.py', '-s', str(shard), 'test'])
+            else:
+                print("Launching shard production {}".format(shard))
+                bot.shards[shard] = subprocess.Popen(['python3', 'dbot.py', '-s', str(shard)])
+            
     
 def clean_shard_servers():
     shard_servers = bot.db.jget('shard_servers', {0: 0})
@@ -40,6 +70,8 @@ def clean_shard_servers():
     bot.db.jset("shard_servers", temp)
 
 def sigterm_handler(_signum, _frame):
+    global RUNNING
+    RUNNING = False
     print("Overseer caught SIGTERM, sleeping for 15!")
     time.sleep(15)
     sys.exit(0)
--- launch.sh
+++ launch.sh
@@ -9,13 +9,6 @@ _term() {
 echo "Launching web"
 python3 webLauncher.py &
 
-for ((i=0; i<$1; i++))
-do
-    echo "Launching shard $2 $i with command python3 dbot.py -s $i $2"
-    python3 dbot.py -s $i $2 &
-    sleep 10
-done
-
 trap _term SIGTERM
 
 python3 beAnnoying.py $2 # don't fall asleep

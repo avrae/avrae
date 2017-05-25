2f8f2570a29ff28d03b039c209d0a8c1cc9e1b94
Andrew Zhu <mommothazaz123@gmail.com>
2017-05-25 00:56:20 -0700

    added delete alias route

--- web/web.py
+++ web/web.py
@@ -87,7 +87,7 @@ def auth():
     session['oauth2_state'] = state
     return redirect(authorization_url)
 
-# -----Web Alias Test-----
+# -----Web Alias Things-----
 
 @app.route('/aliases/list')
 def aliases_list():
@@ -105,6 +105,49 @@ def aliases_list():
     cvars = db.jget('char_vars', {}).get(user_id, {})
     chars = db.jget(user_id + ".characters", {})
     return render_template('aliases/list.html', aliases=aliases, snippets=snippets, cvars=cvars, chars=chars)
+
+@app.route('/aliases/delete', methods=['POST'])
+def aliases_delete():
+    if not 'oauth2_token' in session:
+        session['original_page'] = ".aliases_list"
+        return redirect(url_for(".auth"))
+    discord = make_session(token=session.get('oauth2_token'))
+    resp = discord.get(API_BASE_URL + '/users/@me')
+    if resp.status_code == 401:
+        session['original_page'] = ".aliases_list"
+        return redirect(url_for(".auth"))
+    user_id = resp.json().get('id')
+    alias_type = request.values.get('type')
+    alias_name = request.values.get('name')
+    if alias_type == 'alias':
+        aliases = db.jget('cmd_aliases', {})
+        user_aliases = aliases.get(user_id, {})
+        try:
+            del user_aliases[alias_name]
+        except KeyError:
+            return "Alias not found", 404
+        aliases[user_id] = user_aliases
+        db.jset('cmd_aliases', aliases)
+    elif alias_type == 'snippet':
+        snippets = db.jget('damage_snippets', {})
+        user_snippets = snippets.get(user_id, {})
+        try:
+            del user_snippets[alias_name]
+        except KeyError:
+            return "Alias not found", 404
+        snippets[user_id] = user_snippets
+        db.jset('damage_snippets', snippets)
+    else: # "cvar-cid"
+        cvars = db.jget('char_vars', {})
+        cid = '-'.join(alias_type.split('-')[1:])
+        char_cvars = cvars.get(user_id, {}).get(cid, {})
+        try:
+            del char_cvars[alias_name]
+        except KeyError:
+            return "Alias not found", 404
+        cvars[user_id][cid] = char_cvars
+        db.jset('char_vars', cvars)
+    return "Alias deleted"
 
 # -----Tests-----
 

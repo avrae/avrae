a61611eb0fa96e54f0dec36f9778b6e730bf72f4
Andrew Zhu <mommothazaz123@gmail.com>
2017-06-03 20:41:41 -0700

    added timestamp to dicecloud logs

--- utils/loggers.py
+++ utils/loggers.py
@@ -3,6 +3,7 @@ Created on Jun 3, 2017
 
 @author: andrew
 '''
+from datetime import datetime
 
 class TextLogger:
     
@@ -10,10 +11,11 @@ class TextLogger:
         self.file = file
         
     def text_log(self, ctx, message):
+        timestamp = datetime.today().isoformat() + " "
         try:
             log = "channel {} ({}), server {} ({})\nmessage: {}\n----------\n".format(ctx.message.channel, ctx.message.channel.id, ctx.message.server, ctx.message.server.id, ctx.message.content)
         except AttributeError:
             log = "PM with {} ({})\nmessage: {}\n----------\n".format(ctx.message.author, ctx.message.author.id, ctx.message.content)
         with open(self.file, mode='a', encoding='utf-8') as f:
-            f.write(message + log)
+            f.write(timestamp + message + log)
             print(message + log)

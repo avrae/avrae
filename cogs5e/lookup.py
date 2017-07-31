36ccbe8d2842b7f60d838bb386cc009b6fe32c9c
Andrew Zhu <mommothazaz123@gmail.com>
2017-07-31 17:09:27 +0800

    completely refactored search

--- cogs5e/lookup.py
+++ cogs5e/lookup.py
@@ -3,23 +3,16 @@ Created on Nov 29, 2016
 
 @author: andrew
 '''
-
-import asyncio
-import json
-import math
 import random
-import re
 import shlex
 
 import discord
 from discord.ext import commands
 
-from cogs5e.funcs.dice import roll
-from cogs5e.funcs.lookupFuncs import searchCondition, searchMonster, searchSpell, \
-    searchItem, searchRule, searchFeat, searchRacialFeat, searchClassFeat
+from cogs5e.funcs.lookupFuncs import searchCondition, searchRule, searchRacialFeat, searchFeat, \
+    searchClassFeat, searchMonster, getMonster, searchSpell, getSpell, searchItem, getItem
 from utils import checks
-from utils.functions import discord_trim, print_table, list_get, get_positivity, \
-    fuzzywuzzy_search, fuzzywuzzy_search_all
+from utils.functions import discord_trim, get_positivity
 
 
 class Lookup:
@@ -28,6 +21,29 @@ class Lookup:
     def __init__(self, bot):
         self.bot = bot
         self.settings = self.bot.db.not_json_get("lookup_settings", {}) if bot is not None else {}
+
+    async def get_selection(self, results, ctx, returns_object=True):
+        results = results[:10] # sanity
+        if returns_object: names = [r['name'] for r in results]
+        else: names = results
+        embed = discord.Embed()
+        embed.title = "Multiple Matches Found"
+        selectStr = " Which one were you looking for? (Type the number, or \"c\" to cancel)\n"
+        for i, r in enumerate(names):
+            selectStr += f"**[{i+1}]** - {r}\n"
+        embed.description = selectStr
+        embed.color = random.randint(0, 0xffffff)
+        selectMsg = await self.bot.send_message(ctx.message.channel, embed=embed)
+
+        def chk(msg):
+            valid = [str(v) for v in range(1, len(results) + 1)] + ["c"]
+            return msg.content in valid
+
+        m = await self.bot.wait_for_message(timeout=30, author=ctx.message.author, channel=selectMsg.channel,
+                                            check=chk)
+
+        if m is None or m.content == "c": return None
+        return results[int(m.content) - 1]
             
     @commands.command(pass_context=True, aliases=['status'])
     async def condition(self, ctx, *, name : str):
@@ -37,20 +53,22 @@ class Lookup:
             pm = self.settings.get(guild_id, {}).get("pm_result", False)    
         except:
             pm = False
+        destination = ctx.message.author if pm else ctx.message.channel
         
-        result = searchCondition(name, search=fuzzywuzzy_search_all)
+        result = searchCondition(name)
         if result is None:
             return await self.bot.say('Condition not found.')
+        strict = result[1]
+        results = result[0]
         
-        top = result[0]
-        top_score = top[1]
-        top_key = top[0]
-        if top_score < 60:
-            results = "Condition not found! Did you mean:\n"
-            results += '\n'.join("{0} ({1}% match)".format(a[0], a[1]) for a in result)
-            return await self.bot.say(results)
+        if strict:
+            result = results
         else:
-            result = searchCondition(top_key)
+            if len(results) == 1:
+                result = results[0]
+            else:
+                result = await self.get_selection(results, ctx)
+                if result is None: return await self.bot.say('Selection timed out or was cancelled.')
         
         conName = result['name']
         conHeader = '-' * len(conName)
@@ -59,34 +77,32 @@ class Lookup:
 
         # do stuff here
         for r in discord_trim(out):
-            if pm:
-                await self.bot.send_message(ctx.message.author, r)
-            else:
-                await self.bot.say(r)
+            await self.bot.send_message(destination, r)
                 
     @commands.command(pass_context=True)
     async def rule(self, ctx, *, name : str):
         """Looks up a rule."""
         try:
-            guild_id = ctx.message.server.id 
-            pm = self.settings.get(guild_id, {}).get("pm_result", False)    
+            guild_id = ctx.message.server.id
+            pm = self.settings.get(guild_id, {}).get("pm_result", False)
         except:
             pm = False
-        
-        result = searchRule(name, search=fuzzywuzzy_search_all)
+
+        result = searchRule(name)
         if result is None:
-            return await self.bot.say('Rule not found. PM the bot author if you think this rule is missing.')
-        
-        top = result[0]
-        top_score = top[1]
-        top_key = top[0]
-        if top_score < 60:
-            results = "Rule not found! Did you mean:\n"
-            results += '\n'.join("{0} ({1}% match)".format(a[0], a[1]) for a in result)
-            return await self.bot.say(results)
+            return await self.bot.say('Rule not found.')
+        strict = result[1]
+        results = result[0]
+
+        if strict:
+            result = results
         else:
-            result = searchRule(top_key)
-        
+            if len(results) == 1:
+                result = results[0]
+            else:
+                result = await self.get_selection(results, ctx)
+                if result is None: return await self.bot.say('Selection timed out or was cancelled.')
+
         conName = result['name']
         conHeader = '-' * len(conName)
         conDesc = result['desc']
@@ -98,30 +114,31 @@ class Lookup:
                 await self.bot.send_message(ctx.message.author, r)
             else:
                 await self.bot.say(r)
-                
+
     @commands.command(pass_context=True)
     async def feat(self, ctx, *, name : str):
         """Looks up a feat."""
         try:
-            guild_id = ctx.message.server.id 
-            pm = self.settings.get(guild_id, {}).get("pm_result", False)    
+            guild_id = ctx.message.server.id
+            pm = self.settings.get(guild_id, {}).get("pm_result", False)
         except:
             pm = False
-        
-        result = searchFeat(name, search=fuzzywuzzy_search_all)
+
+        result = searchFeat(name)
         if result is None:
             return await self.bot.say('Feat not found.')
-        
-        top = result[0]
-        top_score = top[1]
-        top_key = top[0]
-        if top_score < 60:
-            results = "Feat not found! Did you mean:\n"
-            results += '\n'.join("{0} ({1}% match)".format(a[0], a[1]) for a in result)
-            return await self.bot.say(results)
+        strict = result[1]
+        results = result[0]
+
+        if strict:
+            result = results
         else:
-            result = searchFeat(top_key)
-        
+            if len(results) == 1:
+                result = results[0]
+            else:
+                result = await self.get_selection(results, ctx)
+                if result is None: return await self.bot.say('Selection timed out or was cancelled.')
+
         if isinstance(result['text'], list):
             result['text'] = '\n'.join(t for t in result.get('text', []) if t is not None and not t.startswith('Source:'))
         result['prerequisite'] = result.get('prerequisite') or "None"
@@ -133,30 +150,31 @@ class Lookup:
                 await self.bot.send_message(ctx.message.author, r)
             else:
                 await self.bot.say(r)
-                
+
     @commands.command(pass_context=True)
     async def racefeat(self, ctx, *, name : str):
         """Looks up a racial feature."""
         try:
-            guild_id = ctx.message.server.id 
-            pm = self.settings.get(guild_id, {}).get("pm_result", False)    
+            guild_id = ctx.message.server.id
+            pm = self.settings.get(guild_id, {}).get("pm_result", False)
         except:
             pm = False
-        
-        result = searchRacialFeat(name, search=fuzzywuzzy_search_all)
+
+        result = searchRacialFeat(name)
         if result is None:
-            return await self.bot.say('Racial feature not found.')
-        
-        top = result[0]
-        top_score = top[1]
-        top_key = top[0]
-        if top_score < 60:
-            results = "Racial feature not found! Did you mean:\n"
-            results += '\n'.join("{0} ({1}% match)".format(a[0], a[1]) for a in result)
-            return await self.bot.say(results)
+            return await self.bot.say('Condition not found.')
+        strict = result[1]
+        results = result[0]
+
+        if strict:
+            result = results
         else:
-            result = searchRacialFeat(top_key)
-        
+            if len(results) == 1:
+                result = results[0]
+            else:
+                result = await self.get_selection(results, ctx)
+                if result is None: return await self.bot.say('Selection timed out or was cancelled.')
+
         if isinstance(result['text'], list):
             result['text'] = '\n'.join(t for t in result.get('text', []) if t is not None)
         out = "**{name}**\n{text}".format(**result)
@@ -167,30 +185,31 @@ class Lookup:
                 await self.bot.send_message(ctx.message.author, r)
             else:
                 await self.bot.say(r)
-                
+
     @commands.command(pass_context=True)
     async def classfeat(self, ctx, *, name : str):
         """Looks up a class feature."""
         try:
-            guild_id = ctx.message.server.id 
-            pm = self.settings.get(guild_id, {}).get("pm_result", False)    
+            guild_id = ctx.message.server.id
+            pm = self.settings.get(guild_id, {}).get("pm_result", False)
         except:
             pm = False
-        
-        result = searchClassFeat(name, search=fuzzywuzzy_search_all)
+
+        result = searchClassFeat(name)
         if result is None:
-            return await self.bot.say('Class feature not found.')
-        
-        top = result[0]
-        top_score = top[1]
-        top_key = top[0]
-        if top_score < 60:
-            results = "Class feature not found! Did you mean:\n"
-            results += '\n'.join("{0} ({1}% match)".format(a[0], a[1]) for a in result)
-            return await self.bot.say(results)
+            return await self.bot.say('Condition not found.')
+        strict = result[1]
+        results = result[0]
+
+        if strict:
+            result = results
         else:
-            result = searchClassFeat(top_key)
-        
+            if len(results) == 1:
+                result = results[0]
+            else:
+                result = await self.get_selection(results, ctx)
+                if result is None: return await self.bot.say('Selection timed out or was cancelled.')
+
         if isinstance(result['text'], list):
             result['text'] = '\n'.join(t for t in result.get('text', []) if t is not None)
         out = "**{name}**\n{text}".format(**result)
@@ -201,7 +220,7 @@ class Lookup:
                 await self.bot.send_message(ctx.message.author, r)
             else:
                 await self.bot.say(r)
-    
+
     @commands.command(pass_context=True, no_pm=True)
     @checks.admin_or_permissions(manage_server=True)
     async def lookup_settings(self, ctx, *, args:str):
@@ -230,19 +249,19 @@ class Lookup:
             setting = get_positivity(setting)
             guild_settings['pm_result'] = setting if setting is not None else False
             out += 'pm_result set to {}!\n'.format(str(guild_settings['pm_result']))
-            
+
         self.settings[guild_id] = guild_settings
         self.bot.db.not_json_set("lookup_settings", self.settings)
         await self.bot.say("Lookup settings set:\n" + out)
-    
+
     @commands.command(pass_context=True)
     async def monster(self, ctx, *, monstername : str):
         """Looks up a monster.
         Generally requires a Game Master role to show full stat block.
         Game Master Roles: GM, DM, Game Master, Dungeon Master"""
-        
+
         try:
-            guild_id = ctx.message.server.id   
+            guild_id = ctx.message.server.id
             pm = self.settings.get(guild_id, {}).get("pm_result", False)
             visible_roles = ['gm', 'game master', 'dm', 'dungeon master']
             if self.settings.get(guild_id, {}).get("req_dm_monster", True):
@@ -255,88 +274,105 @@ class Lookup:
         except:
             visible = True
             pm = False
-        
-        result = searchMonster(monstername, visible=visible, search=fuzzywuzzy_search_all)
+
         self.bot.botStats["monsters_looked_up_session"] += 1
         self.bot.db.incr('monsters_looked_up_life')
-        
-        top = result[0]
-        top_score = top[1]
-        top_key = top[0]
-        if top_score < 60:
-            results = "Monster not found! Did you mean:\n"
-            results += '\n'.join("{0} ({1}% match)".format(a[0], a[1]) for a in result)
-            return await self.bot.say(results)
+
+        result = searchMonster(monstername)
+        if result is None:
+            return await self.bot.say('Monster not found.')
+        strict = result[1]
+        results = result[0]
+
+        if strict:
+            result = results
         else:
-            result = searchMonster(top_key, visible=visible)
-    
+            if len(results) == 1:
+                result = results[0]
+            else:
+                result = await self.get_selection(results, ctx, returns_object=False)
+                if result is None: return await self.bot.say('Selection timed out or was cancelled.')
+
+        result = getMonster(result, visible=visible)
+
         # do stuff here
         for r in result:
             if pm:
                 await self.bot.send_message(ctx.message.author, r)
             else:
                 await self.bot.say(r)
-            
+
     @commands.command(pass_context=True)
-    async def spell(self, ctx, *, args : str):
+    async def spell(self, ctx, *, name : str):
         """Looks up a spell."""
-        valid_args = {'--class', '--level', '--school'}
-        
+
         try:
-            guild_id = ctx.message.server.id 
-            pm = self.settings.get(guild_id, {}).get("pm_result", False)    
+            guild_id = ctx.message.server.id
+            pm = self.settings.get(guild_id, {}).get("pm_result", False)
         except:
             pm = False
-        
-        result = searchSpell(args, search=fuzzywuzzy_search_all)
+
         self.bot.botStats["spells_looked_up_session"] += 1
         self.bot.db.incr('spells_looked_up_life')
-        
-        top = result[0]
-        top_score = top[1]
-        top_key = top[0]
-        if top_score < 60:
-            results = "Spell not found! Did you mean:\n"
-            results += '\n'.join("{0} ({1}% match)".format(a[0], a[1]) for a in result)
-            return await self.bot.say(results)
+
+        result = searchSpell(name)
+        if result is None:
+            return await self.bot.say('Spell not found.')
+        strict = result[1]
+        results = result[0]
+
+        if strict:
+            result = results
         else:
-            result = searchSpell(top_key)
-        
+            if len(results) == 1:
+                result = results[0]
+            else:
+                result = await self.get_selection(results, ctx, returns_object=False)
+                if result is None: return await self.bot.say('Selection timed out or was cancelled.')
+
+        result = getSpell(result)
+
         for r in result:
             if pm:
                 await self.bot.send_message(ctx.message.author, r)
             else:
                 await self.bot.say(r)
-                
+
     @commands.command(pass_context=True, name='item')
-    async def item_lookup(self, ctx, *, itemname):
+    async def item_lookup(self, ctx, *, name):
         """Looks up an item."""
         try:
-            guild_id = ctx.message.server.id 
-            pm = self.settings.get(guild_id, {}).get("pm_result", False)    
+            guild_id = ctx.message.server.id
+            pm = self.settings.get(guild_id, {}).get("pm_result", False)
         except:
             pm = False
-        
-        result = searchItem(itemname, search=fuzzywuzzy_search_all)
+
         self.bot.botStats["items_looked_up_session"] += 1
         self.bot.db.incr('items_looked_up_life')
-        
-        top = result[0]
-        top_score = top[1]
-        top_key = top[0]
-        if top_score < 60:
-            results = "Item not found! Did you mean:\n"
-            results += '\n'.join("{0} ({1}% match)".format(a[0], a[1]) for a in result)
-            return await self.bot.say(results)
+
+        result = searchItem(name)
+        if result is None:
+            return await self.bot.say('Item not found.')
+        strict = result[1]
+        results = result[0]
+
+        if strict:
+            result = results
         else:
-            result = searchItem(top_key)
+            if len(results) == 1:
+                result = results[0]
+            else:
+                result = await self.get_selection(results, ctx, returns_object=False)
+                if result is None: return await self.bot.say('Selection timed out or was cancelled.')
+
+        result = getItem(result)
 
         for r in discord_trim(result):
             if pm:
                 await self.bot.send_message(ctx.message.author, r)
             else:
                 await self.bot.say(r)
-                
-    
-    
+
+
+
     

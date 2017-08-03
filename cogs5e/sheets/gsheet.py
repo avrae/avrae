94e1571ce794ffbed4cc21ac6b6772c87029bfbd
Andrew Zhu <mommothazaz123@gmail.com>
2017-08-03 21:16:50 +0800

    bit of formatting

--- cogs5e/sheets/gsheet.py
+++ cogs5e/sheets/gsheet.py
@@ -15,26 +15,25 @@ from cogs5e.sheets.sheetParser import SheetParser
 
 
 class GoogleSheet(SheetParser):
-    
     def __init__(self, url, client):
         self.url = url
         self.character = None
         assert client is not None
         self.client = client
-    
+
     def _gchar(self):
         sheet = self.client.open_by_key(self.url).sheet1
         self.character = sheet
         return sheet
-    
+
     async def get_character(self):
         loop = asyncio.get_event_loop()
         return await loop.run_in_executor(None, self._gchar)
-    
+
     async def get_sheet(self):
         loop = asyncio.get_event_loop()
         return await loop.run_in_executor(None, self._get_sheet)
-    
+
     def _get_sheet(self):
         """Returns a dict with character sheet data."""
         if self.character is None: raise Exception('You must call get_character() first.')
@@ -51,21 +50,21 @@ class GoogleSheet(SheetParser):
             raise MissingAttribute("Max HP")
         except:
             raise
-        
+
         saves = {}
         for key in skills:
             if 'Save' in key:
                 saves[key] = skills[key]
-        
+
         stat_vars = {}
         stat_vars.update(stats)
         stat_vars['level'] = int(level)
         stat_vars['hp'] = hp
         stat_vars['armor'] = int(armor)
         stat_vars.update(saves)
-        
+
         sheet = {'type': 'google',
-                 'version': 3, #v3: added stat cvars
+                 'version': 3,  # v3: added stat cvars
                  'stats': stats,
                  'levels': {'level': int(level)},
                  'hp': hp,
@@ -77,11 +76,11 @@ class GoogleSheet(SheetParser):
                  'vuln': [],
                  'saves': saves,
                  'stat_cvars': stat_vars}
-                
+
         embed = self.get_embed(sheet)
-        
+
         return {'embed': embed, 'sheet': sheet}
-    
+
     def get_embed(self, sheet):
         stats = sheet['stats']
         hp = sheet['hp']
@@ -108,17 +107,18 @@ class GoogleSheet(SheetParser):
                                             "**INT:** {intelligenceSave:+}\n" \
                                             "**WIS:** {wisdomSave:+}\n" \
                                             "**CHA:** {charismaSave:+}".format(**saves))
-        
+
         skillsStr = ''
         tempSkills = {}
         for skill, mod in sorted(skills.items()):
             if 'Save' not in skill:
-                skillsStr += '**{}**: {:+}\n'.format(re.sub(r'((?<=[a-z])[A-Z]|(?<!\A)[A-Z](?=[a-z]))', r' \1', skill), mod)
+                skillsStr += '**{}**: {:+}\n'.format(re.sub(r'((?<=[a-z])[A-Z]|(?<!\A)[A-Z](?=[a-z]))', r' \1', skill),
+                                                     mod)
                 tempSkills[skill] = mod
         sheet['skills'] = tempSkills
-                
+
         embed.add_field(name="Skills", value=skillsStr.title())
-        
+
         tempAttacks = []
         for a in attacks:
             if a is not None:
@@ -126,24 +126,26 @@ class GoogleSheet(SheetParser):
                     bonus = a['attackBonus']
                     tempAttacks.append("**{0}:** +{1} To Hit, {2} damage.".format(a['name'],
                                                                                   bonus,
-                                                                                  a['damage'] if a['damage'] is not None else 'no'))
+                                                                                  a['damage'] if a[
+                                                                                                     'damage'] is not None else 'no'))
                 else:
                     tempAttacks.append("**{0}:** {1} damage.".format(a['name'],
                                                                      a['damage'] if a['damage'] is not None else 'no'))
         if tempAttacks == []:
             tempAttacks = ['No attacks.']
         embed.add_field(name="Attacks", value='\n'.join(tempAttacks))
-        
+
         return embed
-    
+
     def get_stats(self):
         """Returns a dict of stats."""
         if self.character is None: raise Exception('You must call get_character() first.')
         character = self.character
-        stats = {"name":"", "image":"", "description":"",
-                 "strength":10, "dexterity":10, "constitution":10, "wisdom":10, "intelligence":10, "charisma":10,
-                 "strengthMod":0, "dexterityMod":0, "constitutionMod":0, "wisdomMod":0, "intelligenceMod":0, "charismaMod":0,
-                 "proficiencyBonus":0}
+        stats = {"name": "", "image": "", "description": "",
+                 "strength": 10, "dexterity": 10, "constitution": 10, "wisdom": 10, "intelligence": 10, "charisma": 10,
+                 "strengthMod": 0, "dexterityMod": 0, "constitutionMod": 0, "wisdomMod": 0, "intelligenceMod": 0,
+                 "charismaMod": 0,
+                 "proficiencyBonus": 0}
         stats['name'] = character.acell("C6").value or "Unnamed"
         stats['description'] = "The Google sheet does not have a description field."
         try:
@@ -151,31 +153,31 @@ class GoogleSheet(SheetParser):
         except (TypeError, ValueError):
             raise MissingAttribute("Proficiency Bonus")
         stats['image'] = character.acell("C176").value
-        
+
         index = 15
         for stat in ('strength', 'dexterity', 'constitution', 'intelligence', 'wisdom', 'charisma'):
             try:
                 stats[stat] = int(character.acell("C" + str(index)).value)
-                stats[stat + 'Mod'] = int(character.acell("C" + str(index-2)).value)
+                stats[stat + 'Mod'] = int(character.acell("C" + str(index - 2)).value)
                 index += 5
             except (TypeError, ValueError):
                 raise MissingAttribute(stat)
-        
+
         return stats
-            
+
     def get_attack(self, atkIn):
         """Calculates and returns a dict."""
         if self.character is None: raise Exception('You must call get_character() first.')
         character = self.character
-        attack = {'attackBonus': '0', 'damage':'0', 'name': ''}
+        attack = {'attackBonus': '0', 'damage': '0', 'name': ''}
         name_index = "R" + str(32 + atkIn)
         bonus_index = "Y" + str(32 + atkIn)
         damage_index = "AC" + str(32 + atkIn)
-        
+
         attack['name'] = character.acell(name_index).value
         attack['attackBonus'] = character.acell(bonus_index).value
         attack['damage'] = character.acell(damage_index).value
-        
+
         if attack['name'] is "":
             return None
         if attack['damage'] is "":
@@ -191,11 +193,11 @@ class GoogleSheet(SheetParser):
                 attack['damage'] = dice
                 if comment.strip():
                     attack['details'] = comment.strip()
-        
+
         attack['attackBonus'] = attack['attackBonus'].replace('+', '', 1) if attack['attackBonus'] is not '' else None
-        
+
         return attack
-        
+
     def get_attacks(self):
         """Returns a list of dicts of all of the character's attacks."""
         if self.character is None: raise Exception('You must call get_character() first.')
@@ -204,7 +206,7 @@ class GoogleSheet(SheetParser):
             a = self.get_attack(attack)
             if a is not None: attacks.append(a)
         return attacks
-            
+
     def get_skills(self):
         """Returns a dict of all the character's skills."""
         if self.character is None: raise Exception('You must call get_character() first.')
@@ -229,15 +231,15 @@ class GoogleSheet(SheetParser):
                 skills[skillsMap[index]] = int(character.acell(skill).value)
             except (TypeError, ValueError):
                 raise MissingAttribute(skillsMap[index])
-        
+
         return skills
-    
+
     def get_level(self):
         if self.character is None: raise Exception('You must call get_character() first.')
         character = self.character
         level = int(character.acell("AL6").value)
         return level
-    
+
     def get_description(self):
         if self.character is None: raise Exception('You must call get_character() first.')
         character = self.character
@@ -257,6 +259,3 @@ class GoogleSheet(SheetParser):
                            character.acell("I150").value.lower() or "unknown",
                            character.acell("L150").value.lower() or "unknown")
         return desc
-    
-    
-    

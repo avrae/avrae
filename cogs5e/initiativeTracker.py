9681072ca271e2fa452a2d949d4e46a7b363921f
Andrew Zhu <mommothazaz123@gmail.com>
2017-08-07 22:41:39 +0800

    autoadv applies to cadd

--- cogs5e/initiativeTracker.py
+++ cogs5e/initiativeTracker.py
@@ -539,7 +539,10 @@ class InitTracker:
         
         embed = discord.Embed()
         embed.colour = random.randint(0, 0xffffff) if character.get('settings', {}).get('color') is None else character.get('settings', {}).get('color')
-        
+
+        skill_effects = character.get('skill_effects', {})
+        args += ' ' + skill_effects.get(skill, '')  # dicecloud v7 - autoadv
+
         args = shlex.split(args)
         args = parse_args(args)
         adv = 0 if args.get('adv', False) and args.get('dis', False) else 1 if args.get('adv', False) else -1 if args.get('dis', False) else 0

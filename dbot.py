bf1414db30333666f501b98b4ef7dd45332d7096
Andrew Zhu <mommothazaz123@gmail.com>
2017-10-20 08:20:21 -0700

    formatting

--- dbot.py
+++ dbot.py
@@ -53,19 +53,24 @@ Join the official testing server [here](https://discord.gg/pQbd4s6)!
 Love the bot? Donate to me [here](https://www.paypal.me/avrae)! \u2764
 '''
 if not SHARDED:
-    bot = commands.Bot(command_prefix=commands.when_mentioned_or(prefix), description=description, pm_help=True, shard_id=0, shard_count=1, max_messages=500)
+    bot = commands.Bot(command_prefix=commands.when_mentioned_or(prefix), description=description, pm_help=True,
+                       shard_id=0, shard_count=1, max_messages=500)
 else:
-    bot = commands.Bot(command_prefix=commands.when_mentioned_or(prefix), description=description, pm_help=True, shard_id=int(shard_id), shard_count=int(shard_count), max_messages=500)
+    bot = commands.Bot(command_prefix=commands.when_mentioned_or(prefix), description=description, pm_help=True,
+                       shard_id=int(shard_id), shard_count=int(shard_count), max_messages=500)
 bot.prefix = prefix
 bot.remove_command('help')
 bot.testing = TESTING
 
-class Credentials():
+
+class Credentials:
     pass
+
 
 # CREDENTIALS
 try:
     import credentials
+
     bot.credentials = Credentials()
     bot.credentials.testToken = credentials.testToken
     bot.credentials.officialToken = credentials.officialToken
@@ -81,18 +86,19 @@ except ImportError:
     bot.credentials.discord_bots_key = os.environ.get('DISCORD_BOTS_KEY')
     bot.credentials.carbon_key = os.environ.get('CARBON_KEY')
     bot.credentials.test_database_url = os.environ.get('TEST_DATABASE_URL')
-    
+
 bot.db = DataIO() if not TESTING else DataIO(testing=True, test_database_url=bot.credentials.test_database_url)
 
 handler = logging.StreamHandler(sys.stdout)
-handler.setFormatter(logging.Formatter('%(asctime)s s.{}:%(levelname)s:%(name)s: %(message)s'.format(getattr(bot, 'shard_id', 0))))
+handler.setFormatter(
+    logging.Formatter('%(asctime)s s.{}:%(levelname)s:%(name)s: %(message)s'.format(getattr(bot, 'shard_id', 0))))
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 logger.addHandler(handler)
 
 log = logging.getLogger('bot')
 
-#-----COGS-----
+# -----COGS-----
 diceCog = Dice(bot)
 charGenCog = CharGenerator(bot)
 initiativeTrackerCog = InitTracker(bot)
@@ -121,6 +127,7 @@ cogs = [diceCog,
         Stats(bot),
         GameTrack(bot)]
 
+
 @bot.event
 async def on_ready():
     print('Logged in as')
@@ -130,24 +137,31 @@ async def on_ready():
     print('------')
     await enter()
 
+
 async def enter():
     await bot.wait_until_ready()
     appInfo = await bot.application_info()
     bot.owner = appInfo.owner
     bot.botStats = {}
-    statKeys = ["dice_rolled_session", "spells_looked_up_session", "monsters_looked_up_session", "commands_used_session", "dice_rolled_life", "spells_looked_up_life", "monsters_looked_up_life", "commands_used_life", "items_looked_up_life", "items_looked_up_session"]
+    statKeys = ["dice_rolled_session", "spells_looked_up_session", "monsters_looked_up_session",
+                "commands_used_session", "dice_rolled_life", "spells_looked_up_life", "monsters_looked_up_life",
+                "commands_used_life", "items_looked_up_life", "items_looked_up_session"]
     for k in statKeys:
         if not bot.db.exists(k): bot.db.set(k, 0)
         bot.botStats[k] = int(bot.db.get(k, 0))
-    bot.botStats["items_looked_up_session"] = bot.botStats["items_looked_up_session"] = bot.botStats["dice_rolled_session"] = bot.botStats["spells_looked_up_session"] = bot.botStats["monsters_looked_up_session"] = bot.botStats["commands_used_session"] = 0
-    if not bot.db.exists('build_num'): bot.db.set('build_num', 114) # this was added in build 114
+    bot.botStats["items_looked_up_session"] = bot.botStats["items_looked_up_session"] = bot.botStats[
+        "dice_rolled_session"] = bot.botStats["spells_looked_up_session"] = bot.botStats["monsters_looked_up_session"] = \
+    bot.botStats["commands_used_session"] = 0
+    if not bot.db.exists('build_num'): bot.db.set('build_num', 114)  # this was added in build 114
     if getattr(bot, "shard_id", 0) == 0: bot.db.incr('build_num')
     await bot.change_presence(game=discord.Game(name='D&D 5e | !help'))
+
 
 @bot.event
 async def on_resumed():
     log.info('resumed.')
-    
+
+
 @bot.event
 async def on_command_error(error, ctx):
     if isinstance(error, commands.CommandNotFound):
@@ -158,41 +172,56 @@ async def on_command_error(error, ctx):
         return await bot.send_message(ctx.message.channel, str(error))
     tb = ''.join(traceback.format_exception(type(error), error, error.__traceback__))
     if isinstance(error, commands.CheckFailure):
-        await bot.send_message(ctx.message.channel, "Error: Either you do not have the permissions to run this command, the command is disabled, or something went wrong internally.")
+        await bot.send_message(ctx.message.channel,
+                               "Error: Either you do not have the permissions to run this command, the command is disabled, or something went wrong internally.")
         return
-    elif isinstance(error, (commands.MissingRequiredArgument, commands.BadArgument, commands.NoPrivateMessage, ValueError)):
-        return await bot.send_message(ctx.message.channel, "Error: " + str(error) + "\nUse `!help " + ctx.command.qualified_name + "` for help.")
+    elif isinstance(error,
+                    (commands.MissingRequiredArgument, commands.BadArgument, commands.NoPrivateMessage, ValueError)):
+        return await bot.send_message(ctx.message.channel, "Error: " + str(
+            error) + "\nUse `!help " + ctx.command.qualified_name + "` for help.")
     elif isinstance(error, commands.CommandOnCooldown):
-        return await bot.send_message(ctx.message.channel, "This command is on cooldown for {:.1f} seconds.".format(error.retry_after))
+        return await bot.send_message(ctx.message.channel,
+                                      "This command is on cooldown for {:.1f} seconds.".format(error.retry_after))
     elif isinstance(error, CommandInvokeError):
         original = error.original
         if isinstance(original, AvraeException):
             return await bot.send_message(ctx.message.channel, str(original))
         if isinstance(original, Forbidden):
             try:
-                return await bot.send_message(ctx.message.author, "Error: I am missing permissions to run this command. Please make sure I have permission to send messages to <#{}>.".format(ctx.message.channel.id))
+                return await bot.send_message(ctx.message.author,
+                                              "Error: I am missing permissions to run this command. Please make sure I have permission to send messages to <#{}>.".format(
+                                                  ctx.message.channel.id))
             except:
                 pass
         if isinstance(original, NotFound):
-            return await bot.send_message(ctx.message.channel, "Error: I tried to edit or delete a message that no longer exists.")
+            return await bot.send_message(ctx.message.channel,
+                                          "Error: I tried to edit or delete a message that no longer exists.")
         if isinstance(original, ValueError) and str(original) == "No closing quotation":
             return await bot.send_message(ctx.message.channel, "Error: No closing quotation.")
         if isinstance(original, HTTPException):
             if original.response.status == 400:
                 return await bot.send_message(ctx.message.channel, "Error: Message is too long, malformed, or empty.")
             if original.response.status == 500:
-                return await bot.send_message(ctx.message.channel, "Error: Internal server error on Discord's end. Please try again.")
+                return await bot.send_message(ctx.message.channel,
+                                              "Error: Internal server error on Discord's end. Please try again.")
 
-    await bot.send_message(ctx.message.channel, "Error: " + str(error) + "\nThis incident has been reported to the developer.")
+    await bot.send_message(ctx.message.channel,
+                           "Error: " + str(error) + "\nThis incident has been reported to the developer.")
     try:
-        await bot.send_message(bot.owner, "Error in channel {} ({}), server {} ({}), shard {}: {}\nCaused by message: `{}`".format(ctx.message.channel, ctx.message.channel.id, ctx.message.server, ctx.message.server.id, getattr(bot, 'shard_id', 0), repr(error), ctx.message.content))
+        await bot.send_message(bot.owner,
+                               "Error in channel {} ({}), server {} ({}), shard {}: {}\nCaused by message: `{}`".format(
+                                   ctx.message.channel, ctx.message.channel.id, ctx.message.server,
+                                   ctx.message.server.id, getattr(bot, 'shard_id', 0), repr(error),
+                                   ctx.message.content))
     except AttributeError:
-        await bot.send_message(bot.owner, "Error in PM with {} ({}), shard 0: {}\nCaused by message: `{}`".format(ctx.message.author.mention, str(ctx.message.author), repr(error), ctx.message.content))
+        await bot.send_message(bot.owner, "Error in PM with {} ({}), shard 0: {}\nCaused by message: `{}`".format(
+            ctx.message.author.mention, str(ctx.message.author), repr(error), ctx.message.content))
     for o in discord_trim(tb):
         await bot.send_message(bot.owner, o)
     log.error("Error caused by message: `{}`".format(ctx.message.content))
     traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
-                
+
+
 @bot.event
 async def on_message(message):
     if message.author.id in bot.get_cog("AdminUtils").muted:
@@ -208,37 +237,45 @@ async def on_message(message):
         guild_prefix = bot.prefix
     if message.content.startswith(guild_prefix):
         message.content = message.content.replace(guild_prefix, bot.prefix, 1)
-    elif message.content.startswith(bot.prefix): return
-    if message.content.startswith(bot.prefix) and INITIALIZING: return await bot.send_message(message.channel, "Bot is initializing, try again in a few seconds!")
+    elif message.content.startswith(bot.prefix):
+        return
+    if message.content.startswith(bot.prefix) and INITIALIZING: return await bot.send_message(message.channel,
+                                                                                              "Bot is initializing, try again in a few seconds!")
     await bot.process_commands(message)
-    
+
+
 @bot.event
 async def on_command(command, ctx):
     bot.botStats['commands_used_session'] += 1
     bot.db.incr('commands_used_life')
     try:
-        log.debug("Command called in channel {0.message.channel} ({0.message.channel.id}), server {0.message.server} ({0.message.server.id}): {0.message.content}".format(ctx))
+        log.debug(
+            "Command called in channel {0.message.channel} ({0.message.channel.id}), server {0.message.server} ({0.message.server.id}): {0.message.content}".format(
+                ctx))
     except AttributeError:
         log.debug("Command in PM with {0.message.author} ({0.message.author.id}): {0.message.content}".format(ctx))
+
 
 # SIGNAL HANDLING
 def sigterm_handler(_signum, _frame):
     try:
         log.info("Attempting to save combats...")
         bot.get_cog("InitTracker").panic_save()
-    except: pass
-    time.sleep(0.1) # maybe?
+    except:
+        pass
+    time.sleep(0.1)  # maybe?
     sys.exit(0)
-    
+
+
 signal.signal(signal.SIGTERM, sigterm_handler)
-            
+
 for cog in cogs:
     bot.add_cog(cog)
 
 if SHARDED: log.info("I am shard {} of {}.".format(str(int(bot.shard_id) + 1), str(bot.shard_count)))
 
 INITIALIZING = False
-if not TESTING:     
+if not TESTING:
     bot.run(bot.credentials.officialToken)  # official token
 else:
     bot.run(bot.credentials.testToken)  # test token

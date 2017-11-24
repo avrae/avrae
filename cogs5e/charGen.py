7f786382da24c1cc65299cb6b8c458e90a40188e
Andrew Zhu <mommothazaz123@gmail.com>
2017-11-23 19:31:48 -0800

    randchar race/class

--- cogs5e/charGen.py
+++ cogs5e/charGen.py
@@ -1,11 +1,21 @@
 import asyncio
+import logging
 import random
 from string import capwords
 
+from MeteorClient import MeteorClient
 from discord.ext import commands
 
+import credentials
 from cogs5e.funcs.dice import roll
 from cogs5e.funcs.lookupFuncs import c
+from cogs5e.models.embeds import EmbedWithAuthor
+from utils.functions import parse_data_entry, ABILITY_MAP
+
+DICECLOUD_USERNAME = 'avrae'
+DICECLOUD_PASSWORD = credentials.dicecloud_pass.encode()
+
+log = logging.getLogger(__name__)
 
 
 class CharGenerator:
@@ -14,6 +24,24 @@ class CharGenerator:
     def __init__(self, bot):
         self.bot = bot
         self.makingChar = set()
+        self.dicecloud_client = MeteorClient('ws://dicecloud.com/websocket', debug=self.bot.testing)
+        self.dicecloud_client.initialized = False
+        self.bot.loop.create_task(self.auth_dicecloud())
+
+    async def auth_dicecloud(self):
+        self.dicecloud_client.connect()
+        log.info("Connecting to dicecloud")
+        while not self.dicecloud_client.connected:
+            await asyncio.sleep(0.1)
+        self.dicecloud_client.login(DICECLOUD_USERNAME, DICECLOUD_PASSWORD)
+        log.info("Logged in to dicecloud")
+
+        await asyncio.sleep(1)  # wait until users collection has updated
+
+        USER_ID = self.dicecloud_client.find_one('users', selector={'username': DICECLOUD_USERNAME}).get('_id')
+        log.info("User ID: " + USER_ID)
+        self.dicecloud_client.initialized = True
+        self.dicecloud_client.user_id = USER_ID
 
     @commands.command(pass_context=True, name='randchar')
     async def randChar(self, ctx, level="0"):
@@ -164,8 +192,13 @@ class CharGenerator:
         except asyncio.CancelledError:
             pass
 
-    async def genChar(self, ctx, level):
+    async def genChar(self, ctx, final_level):
         loadingMessage = await self.bot.send_message(ctx.message.channel, "Generating character, please wait...")
+
+        features = []
+        race_attrs = {}
+        class_attrs = {}
+        gen_attrs = {}
 
         # Name Gen
         #    DMG name gen
@@ -177,28 +210,151 @@ class CharGenerator:
         await self.bot.send_message(ctx.message.author, "**Stats for {0}:** `{1}`".format(name, stats))
         # Race Gen
         #    Racial Features
-        race = self.raceGen(ctx, level)
-        raceTraits = race[1]
-        race = race[0]
-        raceTraitStr = ''
-        for t in raceTraits:
-            raceTraitStr += "\n\n**{trait}**: {desc}".format(**t)
-        raceTraitStr = "**Racial Traits ({0}):** {1}".format(race, raceTraitStr)
-        result = self.discord_trim(raceTraitStr)
-        for r in result:
-            await self.bot.send_message(ctx.message.author, r)
+        race = random.choice([r for r in c.races if r['source'] in ('PHB', 'VGM')])
+
+        _sizes = {'T': "Tiny", 'S': "Small",
+                  'M': "Medium", 'L': "Large", 'H': "Huge"}
+        embed = EmbedWithAuthor(ctx)
+        embed.title = race['name']
+        embed.description = f"Source: {race.get('source', 'unknown')}"
+        embed.add_field(name="Speed",
+                        value=race['speed'] + ' ft.' if isinstance(race['speed'], str) else \
+                            ', '.join(f"{k} {v} ft." for k, v in race['speed'].items()))
+        embed.add_field(name="Size", value=_sizes.get(race.get('size'), 'unknown'))
+
+        ability = []
+        for k, v in race['ability'].items():
+            if not k == 'choose':
+                ability.append(f"{k} {v}")
+            else:
+                ability.append(f"Choose {v[0]['count']} from {', '.join(v[0]['from'])} {v[0].get('amount', 1)}")
+
+        embed.add_field(name="Ability Bonuses", value=', '.join(ability))
+        if race.get('proficiency'):
+            embed.add_field(name="Proficiencies", value=race.get('proficiency', 'none'))
+
+        traits = []
+        if 'trait' in race:
+            one_rfeats = race.get('trait', [])
+            for rfeat in one_rfeats:
+                temp = {'name': rfeat['name'],
+                        'text': parse_data_entry(rfeat['text'])}
+                traits.append(temp)
+        else:  # assume entries
+            for entry in race['entries']:
+                temp = {'name': entry['name'],
+                        'text': parse_data_entry(entry['entries'])}
+                traits.append(temp)
+
+        for t in traits:
+            f_text = t['text']
+            f_text = [f_text[i:i + 1024] for i in range(0, len(f_text), 1024)]
+            embed.add_field(name=t['name'], value=f_text[0])
+            for piece in f_text[1:]:
+                embed.add_field(name="con't", value=piece)
+
+        await self.bot.send_message(ctx.message.author, embed=embed)
+
         # Class Gen
         #    Class Features
-        classVar = self.classGen(ctx, level)
-        classTraits = classVar[1]
-        classVar = classVar[0]
-        classTraitStr = ''
-        for t in classTraits:
-            classTraitStr += "\n\n**{0}**: {1}".format(t['trait'], t['desc'].replace('<br>', '\n'))
-        classTraitStr = "**Class Traits ({0}):** {1}".format(classVar, classTraitStr)
-        result = self.discord_trim(classTraitStr)
-        for r in result:
-            await self.bot.send_message(ctx.message.author, r)
+        _class = random.choice([cl for cl in c.classes if not 'UA' in cl.get('source')])
+        subclass = random.choice([s for s in _class['subclasses'] if not 'UA' in s['source']])
+        embed = EmbedWithAuthor(ctx)
+        embed.title = f"{_class['name']} ({subclass['name']})"
+        embed.add_field(name="Hit Die", value=f"1d{_class['hd']['faces']}")
+        embed.add_field(name="Saving Throws", value=', '.join(ABILITY_MAP.get(p) for p in _class['proficiency']))
+
+        levels = []
+        starting_profs = f"You are proficient with the following items, " \
+                         f"in addition to any proficiencies provided by your race or background.\n" \
+                         f"Armor: {', '.join(_class['startingProficiencies'].get('armor', ['None']))}\n" \
+                         f"Weapons: {', '.join(_class['startingProficiencies'].get('weapons', ['None']))}\n" \
+                         f"Tools: {', '.join(_class['startingProficiencies'].get('tools', ['None']))}\n" \
+                         f"Skills: Choose {_class['startingProficiencies']['skills']['choose']} from " \
+                         f"{', '.join(_class['startingProficiencies']['skills']['from'])}"
+
+        equip_choices = '\n'.join(f"• {i}" for i in _class['startingEquipment']['default'])
+        gold_alt = f"Alternatively, you may start with {_class['startingEquipment']['goldAlternative']} gp " \
+                   f"to buy your own equipment." if 'goldAlternative' in _class['startingEquipment'] else ''
+        starting_items = f"You start with the following items, plus anything provided by your background.\n" \
+                         f"{equip_choices}\n" \
+                         f"{gold_alt}"
+
+        for level in range(1, 21):
+            level_str = []
+            level_features = _class['classFeatures'][level - 1]
+            for feature in level_features:
+                level_str.append(feature.get('name'))
+            levels.append(', '.join(level_str))
+
+        embed.add_field(name="Starting Proficiencies", value=starting_profs)
+        embed.add_field(name="Starting Equipment", value=starting_items)
+
+        level_features_str = ""
+        for i, l in enumerate(levels):
+            level_features_str += f"`{i+1}` {l}\n"
+        embed.description = level_features_str
+
+        await self.bot.send_message(ctx.message.author, embed=embed)
+
+        embed = EmbedWithAuthor(ctx)
+        level_resources = {}
+        for table in _class['classTableGroups']:
+            relevant_row = table['rows'][final_level - 1]
+            for i, col in enumerate(relevant_row):
+                level_resources[table['colLabels'][i]] = parse_data_entry([col])
+
+        for res_name, res_value in level_resources.items():
+            embed.add_field(name=res_name, value=res_value)
+        await self.bot.send_message(ctx.message.author, embed=embed)
+
+        embed_queue = [EmbedWithAuthor(ctx)]
+        num_subclass_features = 0
+        num_fields = 0
+        char_count = 0
+        def inc_fields(text):
+            nonlocal num_fields
+            nonlocal char_count
+            num_fields += 1
+            char_count += len(text)
+            if num_fields > 25:
+                embed_queue.append(EmbedWithAuthor(ctx))
+                num_fields = 0
+                char_count = 0
+            if char_count > 5800:
+                embed_queue.append(EmbedWithAuthor(ctx))
+                num_fields = 0
+                char_count = 0
+        for level in range(1, final_level + 1):
+            level_features = _class['classFeatures'][level - 1]
+            for f in level_features:
+                if f.get('gainSubclassFeature'):
+                    num_subclass_features += 1
+                text = parse_data_entry(f['entries'])
+                text = [text[i:i + 1024] for i in range(0, len(text), 1024)]
+                inc_fields(text[0])
+                embed_queue[-1].add_field(name=f['name'], value=text[0])
+                for piece in text[1:]:
+                    inc_fields(piece)
+                    embed_queue[-1].add_field(name="con't", value=piece)
+        for num in range(num_subclass_features):
+            level_features = subclass['subclassFeatures'][num]
+            for feature in level_features:
+                for entry in feature.get('entries', []):
+                    if not isinstance(entry, dict): continue
+                    if not entry.get('type') == 'entries': continue
+                    fe = {'name': entry['name'],
+                          'text': parse_data_entry(entry['entries'])}
+                    text = [fe['text'][i:i + 1024] for i in range(0, len(fe['text']), 1024)]
+                    inc_fields(text[0])
+                    embed_queue[-1].add_field(name=fe['name'], value=text[0])
+                    for piece in text[1:]:
+                        inc_fields(piece)
+                        embed_queue[-1].add_field(name="con't", value=piece)
+
+        for embed in embed_queue:
+            await self.bot.send_message(ctx.message.author, embed=embed)
+
         # Background Gen
         #    Inventory/Trait Gen
         background = self.backgroundGen(ctx)

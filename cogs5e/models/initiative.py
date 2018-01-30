633e7e56b4599c08c44142aeb1dad3a4193bf52d
Andrew Zhu <mommothazaz123@gmail.com>
2018-01-29 16:23:14 -0800

    more framework

--- cogs5e/models/initiative.py
+++ cogs5e/models/initiative.py
@@ -1,12 +1,14 @@
 class Combat:
-    def __init__(self, channelId, summaryMsgId, dmId, options):
-        self._channel = channelId
-        self._summary = summaryMsgId
+    def __init__(self, channelId, summaryMsgId, dmId, options, ctx=None):
+        self._channel = channelId  # readonly
+        self._summary = summaryMsgId  # readonly
         self._dm = dmId
-        self._options = options
+        self._options = options  # readonly (?)
         self._combatants = []
         self._round = 0
-        self._current_combatant = None
+        self._turn = 0
+        self._current_index = None
+        self.ctx = ctx
 
     @property
     def channel(self):
@@ -24,21 +26,94 @@ class Combat:
     def options(self):
         return self._options
 
-    @property
+    @property  # private write
     def round_num(self):
         return self._round
 
+    @property  # private write
+    def turn_num(self):
+        return self._turn
+
+    @property  # private write
+    def index(self):
+        return self._current_index
+
+    @property
+    def current_combatant(self):
+        return self.get_combatants()[self.index]
+
+    def get_combatants(self):
+        return self._combatants
+
+    def to_dict(self):
+        return {'channel': self.channel, 'summary': self.summary, 'dm': self.dm, 'options': self.options,
+                'combatants': [c.to_dict() for c in self.get_combatants()], 'turn': self.turn_num,
+                'round': self.round_num, 'current': self.current_combatant}
+
 
 class Combatant:
-    pass
+    def __init__(self, name, controllerId, initMod, hpMax, hp, ac, private, resists, attacks, ctx=None):
+        self._name = name
+        self._controller = controllerId
+        self._mod = initMod  # readonly
+        self._hpMax = hpMax
+        self._hp = hp
+        self._ac = ac
+        self._private = private
+        self._resists = resists
+        self._attacks = attacks
+        self.ctx = ctx
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def controller(self):
+        return self._controller
+
+    @property
+    def initMod(self):
+        return self._mod
+
+    @property
+    def hpMax(self):
+        return self._hpMax
+
+    @property
+    def hp(self):
+        return self._hp
+
+    @property
+    def ac(self):
+        return self._ac
+
+    @property
+    def isPrivate(self):
+        return self._private
+
+    @property
+    def resists(self):
+        return self._resists
+
+    @property
+    def attacks(self):
+        return self._attacks
+
+    def to_dict(self):
+        return {'name': self.name, 'controller': self.controller, 'mod': self.initMod, 'hpMax': self.hpMax,
+                'hp': self.hp, 'ac': self.ac, 'private': self.isPrivate, 'resists': self.resists,
+                'attacks': self.attacks}
 
 
 class MonsterCombatant(Combatant):
-    pass
+    def __init__(self, monster):
+        super(MonsterCombatant, self).__init__()
 
 
 class PlayerCombatant(Combatant):
-    pass
+    def __init__(self, character):
+        super(PlayerCombatant, self).__init__()
 
 
 class CombatantGroup:

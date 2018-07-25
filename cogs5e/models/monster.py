457501d6896e878482c5378d7ec9ca8e3a5cfd66
Andrew Zhu <mommothazaz123@gmail.com>
2018-07-25 10:48:37 +0800

    minor regex edit

--- cogs5e/models/monster.py
+++ cogs5e/models/monster.py
@@ -7,7 +7,7 @@ import html2text
 from utils.functions import a_or_an
 
 AVRAE_ATTACK_OVERRIDES_RE = re.compile(r'<avrae hidden>(.*?)\|([+-]?\d*)\|(.*?)</avrae>', re.IGNORECASE)
-ATTACK_RE = re.compile(r'(?:<i>)?(?:\w+ ){2,4}Attack:(?:</i>)? ([+-]?\d+) to hit, .*?(?:<i>)?'
+ATTACK_RE = re.compile(r'(?:<i>)?(?:\w+ ){1,4}Attack:(?:</i>)? ([+-]?\d+) to hit, .*?(?:<i>)?'
                        r'Hit:(?:</i>)? [+-]?\d+ \((.+?)\) (\w+) damage[., ]??'
                        r'(?:in melee, or [+-]?\d+ \((.+?)\) (\w+) damage at range[,.]?)?'
                        r'(?: or [+-]?\d+ \((.+?)\) (\w+) damage .*?[.,]?)?'

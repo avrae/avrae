7ad09b031030dbe985022b1b42e8f7afe16f9cea
Andrew Zhu <me@andrew-zhu.com>
2018-12-18 12:24:03 -0800

    fix some tome stuff

--- cogs5e/models/homebrew/tome.py
+++ cogs5e/models/homebrew/tome.py
@@ -42,7 +42,7 @@ class Tome:
         data = self.to_dict_no_spells()
 
         await ctx.bot.mdb.tomes.update_one(
-            {"_id": self._id}, {"$set": data}
+            {"_id": self.id}, {"$set": data}
         )
 
     async def set_active(self, ctx):
@@ -51,7 +51,7 @@ class Tome:
             {"$pull": {"active": ctx.message.author.id}}
         )
         await ctx.bot.mdb.tomes.update_one(
-            {"_id": self._id},
+            {"_id": self.id},
             {"$push": {"active": ctx.message.author.id}}
         )
 
@@ -61,14 +61,14 @@ class Tome:
         :param ctx: Context
         :return: Whether the tome is now active on the server.
         """
-        data = await ctx.bot.mdb.tomes.find_one({"_id": self._id}, ["server_active"])
+        data = await ctx.bot.mdb.tomes.find_one({"_id": self.id}, ["server_active"])
         server_active = data.get('server_active', [])
         if ctx.message.server.id in server_active:
             server_active.remove(ctx.message.server.id)
         else:
             server_active.append(ctx.message.server.id)
         await ctx.bot.mdb.tomes.update_one(
-            {"_id": self._id},
+            {"_id": self.id},
             {"$set": {"server_active": server_active}}
         )
         return ctx.message.server.id in server_active

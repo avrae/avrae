9dfb162a96ac088078e0e4d91d18116aeeb17f96
Croebh <icrona@gmail.com>
2019-04-02 20:33:07 -0600

    Add `combatant.attacks` to scripting

--- cogs5e/funcs/scripting/combat.py
+++ cogs5e/funcs/scripting/combat.py
@@ -78,6 +78,7 @@ class SimpleCombatant:
             self.initmod = self._combatant.initMod
             self.temphp = self._combatant.temphp
             self.resists = self._combatant.resists
+            self.attacks = self._combatant.attacks
         else:
             self.ac = None
             self.hp = None
@@ -85,6 +86,7 @@ class SimpleCombatant:
             self.initmod = None
             self.temphp = None
             self.resists = None
+            self.attacks = None
         self.init = self._combatant.init
         self.name = self._combatant.name
         self.note = self._combatant.notes

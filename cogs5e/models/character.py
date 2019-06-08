b0d1e355dab074e24f41b3391286852f0a55f8f9
Andrew Zhu <me@andrew-zhu.com>
2019-06-07 17:53:31 -0700

    fix an issue with null SABs

--- cogs5e/models/character.py
+++ cogs5e/models/character.py
@@ -214,10 +214,14 @@ class Character(Spellcaster):
             out = {}
         else:
             out = self.cvars.copy()
+        if self.spellbook.sab is not None:
+            spell_mod = self.spellbook.sab - self.stats.prof_bonus
+        else:
+            spell_mod = None
         out.update({
             "name": self.name, "armor": self.ac, "description": self.description, "hp": self.max_hp,
             "image": self.image, "level": self.levels.total_level, "proficiencyBonus": self.stats.prof_bonus,
-            "spell": self.spellbook.sab - self.stats.prof_bonus, "color": hex(self.get_color())[2:]
+            "spell": spell_mod, "color": hex(self.get_color())[2:]
         })
         for cls, lvl in self.levels:
             out[f"{cls}Level"] = lvl

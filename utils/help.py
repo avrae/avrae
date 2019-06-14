02edb64371db85eb69bb82f6d4554a92898d1762
Andrew Zhu <me@andrew-zhu.com>
2019-06-13 18:51:04 -0700

    finish help command rewrite

--- utils/help.py
+++ utils/help.py
@@ -1,5 +1,7 @@
+import itertools
+
 import discord
-from discord.ext.commands import HelpCommand
+from discord.ext.commands import Group, HelpCommand
 
 
 class EmbedPaginator:
@@ -115,51 +117,31 @@ class AvraeHelp(HelpCommand):
                "Type {0}{1} command for more info on a command.\n" \
                "You can also type {0}{1} category for more info on a category.".format(self.clean_prefix, command_name)
 
-    def add_indented_commands(self, commands, *, heading, max_size=None):  # todo
-        """Indents a list of commands after the specified heading.
-
-        The formatting is added to the :attr:`paginator`.
-
-        The default implementation is the command name indented by
-        :attr:`indent` spaces, padded to ``max_size`` followed by
-        the command's :attr:`Command.short_doc` and then shortened
-        to fit into the :attr:`width`.
-
-        Parameters
-        -----------
-        commands: Sequence[:class:`Command`]
-            A list of commands to indent for output.
-        heading: :class:`str`
-            The heading to add to the output. This is only added
-            if the list of commands is greater than 0.
-        max_size: Optional[:class:`int`]
-            The max size to use for the gap between indents.
-            If unspecified, calls :meth:`get_max_size` on the
-            commands parameter.
-        """
-
+    def add_commands(self, commands, *, heading):
+        """Adds a list of formatted commands under a field title."""
         if not commands:
             return
 
-        self.paginator.add_line(heading)
-        max_size = max_size or self.get_max_size(commands)
+        self.embed_paginator.add_field(name=heading)
 
-        get_width = discord.utils._string_width
         for command in commands:
-            name = command.name
-            width = max_size - (get_width(name) - len(name))
-            entry = '{0}{1:<{width}} {2}'.format(self.indent * ' ', name, command.short_doc, width=width)
-            self.paginator.add_line(self.shorten_text(entry))
+            if isinstance(command, Group):
+                name = f"**__{command.name}__**"
+            else:
+                name = f"**{command.name}**"
+            entry = f"{name} - {command.short_doc}"
+            self.embed_paginator.extend_field(entry)
 
     async def send(self):
         """A helper utility to send the page output from :attr:`paginator` to the destination."""
         destination = self.get_destination()
         for embed in self.embed_paginator.embeds:
             await destination.send(embed=embed)
-        if destination is not self.context.channel:  # todo
+
+        if not isinstance(self.context.channel, discord.DMChannel):
             await self.context.channel.send("I have sent help to your PMs.")
 
-    def add_command_formatting(self, command):  # todo
+    def add_command_formatting(self, command):
         """A utility function to format the non-indented block of commands and groups.
 
         Parameters
@@ -168,87 +150,78 @@ class AvraeHelp(HelpCommand):
             The command to format.
         """
 
-        if command.description:
-            self.paginator.add_line(command.description, empty=True)
-
         signature = self.get_command_signature(command)
-        self.paginator.add_line(signature, empty=True)
+        self.embed_paginator.add_field(name=signature)
+
+        if command.description:
+            self.embed_paginator.extend_field(command.description)
 
         if command.help:
             try:
-                self.paginator.add_line(command.help, empty=True)
-            except RuntimeError:
+                self.embed_paginator.extend_field(command.help)
+            except ValueError:
                 for line in command.help.splitlines():
-                    self.paginator.add_line(line)
-                self.paginator.add_line()
+                    self.embed_paginator.extend_field(line)
 
     # ===== HelpCommand overrides =====
     def get_destination(self):
         return self.context.author
 
-    async def prepare_help_command(self, ctx, command=None):
-        await super().prepare_help_command(ctx, command)
-
-    async def send_bot_help(self, mapping):  # todo here and below
+    async def send_bot_help(self, mapping):
         ctx = self.context
         bot = ctx.bot
 
         if bot.description:
             # <description> portion
-            self.paginator.add_line(bot.description, empty=True)
+            self.embed_paginator.add_description(bot.description)
 
-        no_category = '\u200b{0.no_category}:'.format(self)
+        no_category = '\u200bUncategorized'
 
-        def get_category(command, *, no_category=no_category):
+        def get_category(command):
             cog = command.cog
-            return cog.qualified_name + ':' if cog is not None else no_category
+            return cog.qualified_name if cog is not None else no_category
 
         filtered = await self.filter_commands(bot.commands, sort=True, key=get_category)
-        max_size = self.get_max_size(filtered)
         to_iterate = itertools.groupby(filtered, key=get_category)
 
         # Now we can add the commands to the page.
         for category, commands in to_iterate:
-            commands = sorted(commands, key=lambda c: c.name) if self.sort_commands else list(commands)
-            self.add_indented_commands(commands, heading=category, max_size=max_size)
+            commands = sorted(commands, key=lambda c: c.name)
+            self.add_commands(commands, heading=category)
 
         note = self.get_ending_note()
         if note:
-            self.paginator.add_line()
-            self.paginator.add_line(note)
+            self.embed_paginator.add_field("More Help", note)
 
         await self.send()
 
     async def send_command_help(self, command):
         self.add_command_formatting(command)
-        self.paginator.close_page()
         await self.send()
 
     async def send_group_help(self, group):
         self.add_command_formatting(group)
 
-        filtered = await self.filter_commands(group.commands, sort=self.sort_commands)
-        self.add_indented_commands(filtered, heading=self.commands_heading)
+        filtered = await self.filter_commands(group.commands, sort=True)
+        self.add_commands(filtered, heading="Subcommands")
 
         if filtered:
             note = self.get_ending_note()
             if note:
-                self.paginator.add_line()
-                self.paginator.add_line(note)
+                self.embed_paginator.add_field("More Help", note)
 
         await self.send()
 
     async def send_cog_help(self, cog):
         if cog.description:
-            self.paginator.add_line(cog.description, empty=True)
+            self.embed_paginator.add_description(cog.description)
 
-        filtered = await self.filter_commands(cog.get_commands(), sort=self.sort_commands)
-        self.add_indented_commands(filtered, heading=self.commands_heading)
+        filtered = await self.filter_commands(cog.get_commands(), sort=True)
+        self.add_commands(filtered, heading="Commands")
 
         note = self.get_ending_note()
         if note:
-            self.paginator.add_line()
-            self.paginator.add_line(note)
+            self.embed_paginator.add_field("More Help", note)
 
         await self.send()
 

7a5d12efdc55f99ae84071d436f52806ae0b7932
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2019-06-18 21:13:49 -0500

    🚀 Deploy to ECR

---
+++ scripts/ecr_push.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+serviceName=avrae-bot
+ecrRepo=avrae/$serviceName
+awsAccountId=$AWS_ACCOUNT_ID
+ecrRegion=$ECR_REGION
+ecsRegion=$ECS_REGION
+travisBuildNumber=$TRAVIS_BUILD_NUMBER
+env=live
+defaultEnvironmentTag=live
+
+docker tag $serviceName $awsAccountId.dkr.ecr.$ecrRegion.amazonaws.com/$ecrRepo:travis-build-$travisBuildNumber
+docker tag $serviceName $awsAccountId.dkr.ecr.$ecrRegion.amazonaws.com/$ecrRepo:$defaultEnvironmentTag
+eval $(aws ecr get-login --region $ecrRegion --no-include-email)
+docker push $awsAccountId.dkr.ecr.$ecrRegion.amazonaws.com/$ecrRepo
+#aws ecs update-service --cluster $serviceName-$env --service $serviceName --force-new-deployment --region $ecsRegion

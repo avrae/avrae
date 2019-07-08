d29f2da89423c3df70033421da4d21a7acccd124
Andrew Zhu <me@andrew-zhu.com>
2019-07-08 13:06:13 -0500

    attempt running tests as root

--- scripts/travis_pytest.sh
+++ scripts/travis_pytest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-.local/bin/pytest --cov=cogs5e --cov=cogsmisc --cov=utils tests/ $DBOT_ARGS
-cp .coverage shared/.coverage
+pytest --cov=cogs5e --cov=cogsmisc --cov=utils tests/ $DBOT_ARGS
+mv .coverage shared/.coverage

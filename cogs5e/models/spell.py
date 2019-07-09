e05ec82e76c7e5319a3daeee70bde4ee39074825
Andrew Zhu <me@andrew-zhu.com>
2019-07-09 12:32:46 -0500

    AVR-337 auto upcast if warlock
    Resolves #655

--- cogs5e/models/spell.py
+++ cogs5e/models/spell.py
@@ -855,17 +855,36 @@ class Spell:
         if not self.level <= l <= 9:
             raise SpellException("Invalid spell level.")
 
-        if not (caster.can_cast(self, l) or i):
-            embed = EmbedWithAuthor(ctx)
-            embed.title = "Cannot cast spell!"
-            embed.description = "Not enough spell slots remaining, or spell not in known spell list!\n" \
-                f"Use `{ctx.prefix}game longrest` to restore all spell slots if this is a character, " \
-                                "or pass `-i` to ignore restrictions."
-            if l > 0:
-                embed.add_field(name="Spell Slots", value=caster.remaining_casts_of(self, l))
-            return {"embed": embed}
-
         if not i:
+            # if I'm a warlock, and I didn't have any slots of this level anyway (#655)
+            # automatically scale up to the next level s.t. our slots are not 0
+            if l == self.level and not caster.spellbook.get_max_slots(l):
+                l = next((sl for sl in range(l, 6) if caster.spellbook.get_max_slots(sl)), l)  # only scale up to l5
+                args['l'] = l
+
+            # can I cast this spell?
+            if not caster.can_cast(self, l):
+                embed = EmbedWithAuthor(ctx)
+                embed.title = "Cannot cast spell!"
+                if not caster.spellbook.get_slots(l):
+                    # out of spell slots
+                    err = f"You don't have enough level {l} slots left! Use `-l <level>` to cast at a different level, " \
+                        f"`{ctx.prefix}g lr` to take a long rest, or `-i` to ignore spell slots!"
+                elif self.name not in caster.spellbook:
+                    # don't know spell
+                    err = f"You don't know this spell! Use `{ctx.prefix}sb add {self.name}` to add it to your spellbook, " \
+                        f"or pass `-i` to ignore restrictions."
+                else:
+                    # ?
+                    err = "Not enough spell slots remaining, or spell not in known spell list!\n" \
+                        f"Use `{ctx.prefix}game longrest` to restore all spell slots if this is a character, " \
+                        f"or pass `-i` to ignore restrictions."
+                embed.description = err
+                if l > 0:
+                    embed.add_field(name="Spell Slots", value=caster.remaining_casts_of(self, l))
+                return {"embed": embed}
+
+            # use resource
             caster.cast(self, l)
 
         # character setup

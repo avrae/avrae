6370f3e818ae1cca36694dbfcf2c97af5924da2a
Freddie <freddie@wafflemonster.org>
2019-08-01 09:53:14 -0700

    🐛 Don't try to start a new New Relic transaction if one is already active

--- utils/newrelic.py
+++ utils/newrelic.py
@@ -4,6 +4,7 @@ import motor.motor_asyncio
 import newrelic.agent
 import newrelic.api.function_trace
 from discord.ext import commands
+from newrelic.api.transaction import current_transaction
 
 application = newrelic.agent.application()
 
@@ -36,6 +37,12 @@ def hook_discord():
     # The normal New Relic API doesn't work here, let's replace the existing `Command.invoke` function with a version
     # that wraps it in a background task transaction
     async def _command_invoke(self, *args, **kwargs):
+        # If there's already a running transaction, don't start a new one. New Relic doesn't handle coroutines very
+        # well.
+        transaction = current_transaction()
+        if transaction:
+            return await self._invoke(*args, **kwargs)
+
         with newrelic.agent.BackgroundTask(application, name='command:%s' % self.name):
             await self._invoke(*args, **kwargs)
 

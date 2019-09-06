92b5b81ca91c71c1a45fab6a060729be929f6716
Andrew Zhu <me@andrew-zhu.com>
2019-09-06 10:50:15 -0700

    reenable ecs push

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 # Usage: bash scripts/deploy.sh [production|nightly]
 environment=${1:-production}
 
-if [$environment == "production"]; then
+if [[ "$environment" = "production" ]]; then
     bash scripts/upload_help.sh
 fi;
 bash scripts/ecr_push.sh "$environment"

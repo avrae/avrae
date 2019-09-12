5008b551930b989e8f175d767a12fb465634d215
Andrew Zhu <me@andrew-zhu.com>
2019-09-12 13:19:11 -0700

    add some more specs

--- notes/character.spec.ts
+++ notes/character.spec.ts
@@ -1,3 +1,4 @@
+import {Attack, BaseStats, Skill, Spellbook, SpellbookSpell} from './shared.spec'
 // TypeScript syntax is oddly satisfying to write schemas in.
 // typescript-json-schema notes/character.spec.ts Character > notes/character.spec.json
 class Character {
@@ -12,15 +13,7 @@ class Character {
     name: string;
     description: string;
     image: string;
-    stats: {
-        prof_bonus: number;
-        strength: number;
-        dexterity: number;
-        constitution: number;
-        intelligence: number;
-        wisdom: number;
-        charisma: number
-    };
+    stats: BaseStats;
     levels: {
         total_level: number;
         classes: { string: number }; // mapping of {class_name: level}
@@ -56,14 +49,7 @@ class Character {
     };
 
     // spellbook: half-static
-    spellbook: {
-        slots: { string: number }; // current slots (dynamic)
-        max_slots: { string: number }; // max slots (static): map of {level: number_of_slots}
-        spells: SpellbookSpell[]; // list of known spells (static)
-        dc: number; // spellcasting DC (static)
-        sab: number; // spell attack bonus (static)
-        caster_level: number; // actually total class level (used for cantrip scaling; static)
-    };
+    spellbook: Spellbook;
 
     // misc metadata
     live: string | null;
@@ -71,22 +57,6 @@ class Character {
     background: string;
 }
 
-class Skill {
-    // value = base_skill + (prof * prof_bonus) + bonus
-    value: number; // the final calculated bonus
-    prof: 0 | 0.5 | 1 | 2;  // none, joat, prof, expertise
-    bonus: number; // misc bonuses
-    adv: null | true | false; // none, adv, dis
-}
-
-class Attack {
-    name: string;
-    bonus: number; // to-hit bonus, can be null
-    damage: string; // damage dice string, can be null
-    details: string; // text to output (attack description)
-    bonus_calc: string; // the bonus calculation (usually for user-added attacks)
-    damage_calc: string; // the damage calculation (usually for user-added attacks)
-}
 
 class Consumable {
     name: string;
@@ -98,10 +68,3 @@ class Consumable {
     live_id: string | null; // metadata for live sync
 }
 
-class SpellbookSpell {
-    name: string;
-    strict: boolean; // whether the spell is homebrew or not
-    level: null; // not implemented: the spell level
-    dc: null; // not implemented: the spell's explicit DC
-    sab: null // not implemented: the spell's explicit SAB
-}

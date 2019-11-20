e6d4a33c845d24fed7dc479ccb633702dd3da58b
Andrew Zhu <me@andrew-zhu.com>
2019-11-20 13:11:38 -0800

    move bestiaries to use sub mixins

--- cogs5e/models/homebrew/bestiary.py
+++ cogs5e/models/homebrew/bestiary.py
@@ -16,7 +16,7 @@ class Bestiary(CommonHomebrewMixin):
                  name: str, monsters: list = None, desc: str = None,
                  **_):
         # metadata - should never change
-        self.id = _id
+        super().__init__(_id)
         self.sha256 = sha256
         self.upstream = upstream
 
@@ -83,11 +83,10 @@ class Bestiary(CommonHomebrewMixin):
 
     @classmethod
     async def from_ctx(cls, ctx):
-        active_bestiary = await ctx.bot.mdb.bestiary_subscriptions.find_one(
-            {"type": "active", "subscriber_id": ctx.author.id})
+        active_bestiary = await cls.active_id(ctx)
         if active_bestiary is None:
             raise NoActiveBrew()
-        return await cls.from_id(ctx, active_bestiary['object_id'])
+        return await cls.from_id(ctx, active_bestiary)
 
     @classmethod
     async def from_id(cls, ctx, oid):
@@ -122,54 +121,51 @@ class Bestiary(CommonHomebrewMixin):
         result = await ctx.bot.mdb.bestiaries.insert_one(data)
         self.id = result.inserted_id
 
-    async def set_active(self, ctx):
-        """Sets the bestiary as active for the contextual author."""
-        await ctx.bot.mdb.bestiary_subscriptions.delete_many(
-            {"type": "active", "subscriber_id": ctx.author.id}
-        )
-        await ctx.bot.mdb.bestiary_subscriptions.insert_one(
-            {"type": "active", "subscriber_id": ctx.author.id, "object_id": self.id}
-        )
-        return self
+    async def delete(self, ctx):
+        await ctx.bot.mdb.bestiaries.delete_one({"_id": self.id})
+        await self.remove_all_tracking(ctx)
 
-    async def toggle_server_active(self, ctx):
+    # ==== subscriber helpers ====
+    @staticmethod
+    def sub_coll(ctx):
+        return ctx.bot.mdb.bestiary_subscriptions
+
+    async def set_server_active(self, ctx):
         """
-        Toggles whether the bestiary should be active on the contextual server.
-        :param ctx: Context
-        :return: Whether the bestiary is now active on the server.
+        Sets the object as active for the contextual guild.
+        This override is here because bestiaries' server active docs need a provider id.
         """
-        sub_query = {"type": "server_active", "subscriber_id": ctx.guild.id}
-        sub_doc = await ctx.bot.mdb.bestiary_subscriptions.find_one(sub_query)
-
-        if sub_doc is not None:  # I subscribed and want to unsubscribe
-            await ctx.bot.mdb.bestiary_subscriptions.delete_one(sub_query)
-            return False
-        else:  # no one has served this bestiary and I want to
-            sub_doc = {"type": "server_active", "subscriber_id": ctx.guild.id,
-                       "object_id": self.id, "provider_id": ctx.author.id}
-            await ctx.bot.mdb.bestiary_subscriptions.insert_one(sub_doc)
-            return True
-
-    async def subscribe(self, ctx):
-        await ctx.bot.mdb.bestiary_subscriptions.insert_one(
-            {"type": "subscribe", "subscriber_id": ctx.author.id, "object_id": self.id}
-        )
+        sub_doc = {"type": "server_active", "subscriber_id": ctx.guild.id,
+                   "object_id": self.id, "provider_id": ctx.author.id}
+        await self.sub_coll(ctx).insert_one(sub_doc)
 
     async def unsubscribe(self, ctx):
+        """The unsubscribe operation for bestiaries actually acts as a delete operation."""
         # unsubscribe me
-        await ctx.bot.mdb.bestiary_subscriptions.delete_many(
-            {"type": "subscribe", "subscriber_id": ctx.author.id, "object_id": self.id}
-        )
+        await super().unsubscribe(ctx)
 
         # remove all server subs that I provide
-        await ctx.bot.mdb.bestiary_subscriptions.delete_many(
+        await self.sub_coll(ctx).delete_many(
             {"type": "server_active", "provider_id": ctx.author.id, "object_id": self.id}
         )
 
         # if no one is subscribed to this bestiary anymore, delete it.
-        if not await ctx.bot.mdb.bestiary_subscriptions.count_documents({"type": "subscribe", "object_id": self.id}):
+        if not await self.num_subscribers(ctx):
             await self.delete(ctx)
 
+    @staticmethod
+    async def user_bestiaries(ctx):
+        """Returns an async iterator of partial Bestiary objects that the user has imported."""
+        async for b in Bestiary.my_sub_ids(ctx):
+            yield await Bestiary.from_id(ctx, b)
+
+    @staticmethod
+    async def server_bestiaries(ctx):
+        """Returns an async iterator of partial Bestiary objects that are active on the server."""
+        async for b in Bestiary.guild_active_ids(ctx):
+            yield await Bestiary.from_id(ctx, b)
+
+    # ==== bestiary-specific database helpers ====
     async def server_subscriptions(self, ctx):
         """Returns a list of server ids (ints) representing server subscriptions supplied by the contextual author.
         Mainly used to determine what subscriptions should be carried over to a new bestiary when updated."""
@@ -189,33 +185,12 @@ class Bestiary(CommonHomebrewMixin):
         if sub_docs:
             await ctx.bot.mdb.bestiary_subscriptions.insert_many(sub_docs)
 
-    async def delete(self, ctx):
-        await ctx.bot.mdb.bestiaries.delete_one({"_id": self.id})
-        await self.remove_all_tracking(ctx)
-
-    async def remove_all_tracking(self, ctx):
-        await ctx.bot.mdb.bestiary_subscriptions.delete_many({"object_id": self.id})
-
     @staticmethod
     async def num_user(ctx):
         """Returns the number of bestiaries a user has imported."""
         return await ctx.bot.mdb.bestiary_subscriptions.count_documents(
             {"type": "subscribe", "subscriber_id": ctx.author.id}
         )
-
-    @staticmethod
-    async def user_bestiaries(ctx):
-        """Returns an async iterator of partial Bestiary objects that the user has imported."""
-        async for b in ctx.bot.mdb.bestiary_subscriptions.find(
-                {"type": "subscribe", "subscriber_id": ctx.author.id}):
-            yield await Bestiary.from_id(ctx, b['object_id'])
-
-    @staticmethod
-    async def server_bestiaries(ctx):
-        """Returns an async iterator of partial Bestiary objects that are active on the server."""
-        async for b in ctx.bot.mdb.bestiary_subscriptions.find(
-                {"type": "server_active", "subscriber_id": ctx.guild.id}):
-            yield await Bestiary.from_id(ctx, b['object_id'])
 
     async def get_server_sharer(self, ctx):
         """Returns the user ID of the user who shared this bestiary with the server."""
--- cogs5e/models/homebrew/mixins.py
+++ cogs5e/models/homebrew/mixins.py
@@ -1,60 +1,108 @@
-class MixinBase:
+import abc
+
+
+class MixinBase(abc.ABC):
+    def __init__(self, oid):
+        """
+        :type oid: :class:`bson.ObjectId`
+        """
+        self.id = oid  # subscribable objects need to know their ObjectId
+
     @staticmethod
     def sub_coll(ctx):
         """Gets the MongoDB collection used to track subscriptions."""
-        raise NotImplemented
+        raise NotImplementedError
 
     async def remove_all_tracking(self, ctx):
         """Removes all subscriber documents associated with this object."""
-        pass
+        await self.sub_coll(ctx).delete_many({"object_id": self.id})
 
 
-class SubscriberMixin(MixinBase):
+class SubscriberMixin(MixinBase, abc.ABC):
     """A mixin that offers subscription support."""
 
     async def subscribe(self, ctx):
         """Adds the contextual author as a subscriber."""
-        pass
+        await self.sub_coll(ctx).insert_one(
+            {"type": "subscribe", "subscriber_id": ctx.author.id, "object_id": self.id}
+        )
 
     async def unsubscribe(self, ctx):
         """Removes the contextual author from subscribers."""
-        pass
+        await self.sub_coll(ctx).delete_many(
+            {"type": "subscribe", "subscriber_id": ctx.author.id, "object_id": self.id}
+        )
 
     async def num_subscribers(self, ctx):
         """Returns the number of subscribers."""
-        pass
+        return await self.sub_coll(ctx).count_documents({"type": "subscribe", "object_id": self.id})
+
+    @classmethod
+    async def my_sub_ids(cls, ctx):
+        """Returns an async iterator of ObjectIds representing objects the contextual author is subscribed to."""
+        async for sub in cls.sub_coll(ctx).find({"type": "subscribe", "subscriber_id": ctx.author.id}):
+            yield sub['object_id']
 
 
-class ActiveMixin(MixinBase):
+class ActiveMixin(MixinBase, abc.ABC):
     """A mixin that offers user active support."""
 
     async def set_active(self, ctx):
-        """Sets the object as active for the contextual author."""
-        pass
+        """Sets the object as active for the contextual author, and removes active status from any other documents."""
+        await self.sub_coll(ctx).delete_many(
+            {"type": "active", "subscriber_id": ctx.author.id}
+        )
+        await self.sub_coll(ctx).insert_one(
+            {"type": "active", "subscriber_id": ctx.author.id, "object_id": self.id}
+        )
+
+    @classmethod
+    async def active_id(cls, ctx):
+        """Returns the ObjectId of the object the user has set as active, or None if the user does not have any."""
+        obj = await cls.sub_coll(ctx).find_one({"type": "active", "subscriber_id": ctx.author.id})
+        if obj is not None:
+            return obj['object_id']
+        return None
 
 
-class GuildActiveMixin(MixinBase):
+class GuildActiveMixin(MixinBase, abc.ABC):
     """A mixin that offers guild active support."""
 
     async def toggle_server_active(self, ctx):
-        pass
+        """Toggles whether the object is active in the contextual guild.
+        Returns a bool representing its new activity."""
+        sub_doc = await self.sub_coll(ctx).find_one({"type": "server_active", "subscriber_id": ctx.guild.id})
+
+        if sub_doc is not None:  # I subscribed and want to unsubscribe
+            await self.unset_server_active(ctx)
+            return False
+        else:  # no one has served this object and I want to
+            await self.set_server_active(ctx)
+            return True
 
     async def set_server_active(self, ctx):
         """Sets the object as active for the contextual guild."""
-        pass
+        sub_doc = {"type": "server_active", "subscriber_id": ctx.guild.id, "object_id": self.id}
+        await self.sub_coll(ctx).insert_one(sub_doc)
 
     async def unset_server_active(self, ctx):
         """Sets the object as inactive for the contextual guild."""
-        pass
+        await self.sub_coll(ctx).delete_one({"type": "server_active", "subscriber_id": ctx.guild.id})
+
+    @classmethod
+    async def guild_active_ids(cls, ctx):
+        """Returns a async iterator of ObjectIds representing the objects active in the contextual server."""
+        async for sub in cls.sub_coll(ctx).find({"type": "server_active", "subscriber_id": ctx.guild.id}):
+            yield sub['object_id']
 
 
-class EditorMixin(MixinBase):
+class EditorMixin(MixinBase, abc.ABC):
     """A mixin that offers editor tracking."""
     pass
 
 
 # ==== utilities ====
-class CommonHomebrewMixin(SubscriberMixin, ActiveMixin, GuildActiveMixin):
+class CommonHomebrewMixin(SubscriberMixin, ActiveMixin, GuildActiveMixin, abc.ABC):
     pass
 
 # ==== notes ====

7034652f61b78ba45234b2e4ec69f9172d70dad4
Chris A <superturtle41@gmail.com>
2020-09-16 15:25:04 -0400

    Move Attack sort from str to initalization (#1249)
    * Move Attack sort from str to initalization
    
    * revert attack sorting
    
    This reverts #1248

--- cogs5e/models/sheet/attack.py
+++ cogs5e/models/sheet/attack.py
@@ -76,7 +76,7 @@ class AttackList:
 
     # utils
     def build_str(self, caster):
-        return '\n'.join(sorted((atk.build_str(caster) for atk in self.attacks), key=str.lower))
+        return '\n'.join(atk.build_str(caster) for atk in self.attacks)
 
     def __str__(self):
         return '\n'.join(str(atk) for atk in self.attacks)

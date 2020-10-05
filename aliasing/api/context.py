cccf0bc4998e954c8b6a4effb7bc715fbdbebe6b
Chris A <1drturtle@gmail.com>
2020-10-05 17:41:51 -0400

    Expose alias name in scripting

--- aliasing/api/context.py
+++ aliasing/api/context.py
@@ -17,6 +17,7 @@ class AliasContext:
         self._channel = AliasChannel(ctx.channel)
         self._author = AliasAuthor(ctx.author)
         self._prefix = ctx.prefix
+        self._invoked_with = ctx.invoked_with
 
     @property
     def guild(self):
@@ -54,8 +55,18 @@ class AliasContext:
         """
         return self._prefix
 
+    @property
+    def invoked_with(self):
+        """
+        The name the alias was invoked with.
+
+        :rtype: str
+        """
+        return self._invoked_with
+
     def __repr__(self):
-        return f"<AliasContext guild={self.guild} channel={self.channel} author={self.author} prefix={self.prefix}>"
+        return f"<AliasContext guild={self.guild} channel={self.channel} author={self.author} prefix={self.prefix} " \
+               f"name={self.name}>"
 
 
 class AliasGuild:

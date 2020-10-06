f6c34d559c628a3abff358c025d98c894a2d7fe6
Chris A <1drturtle@gmail.com>
2020-10-06 13:10:17 -0400

    Change REPL name

--- aliasing/api/context.py
+++ aliasing/api/context.py
@@ -66,7 +66,7 @@ class AliasContext:
 
     def __repr__(self):
         return f"<AliasContext guild={self.guild} channel={self.channel} author={self.author} prefix={self.prefix}" \
-               f" name={self.invoked_with}>"
+               f" invoked_with={self.invoked_with}>"
 
 
 class AliasGuild:

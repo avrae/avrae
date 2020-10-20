dd6df0f50771ab22f13dad3bc128cf7be01c97c1
Chris A <1drturtle@gmail.com>
2020-10-19 23:58:36 -0400

    update create_cc

--- aliasing/api/character.py
+++ aliasing/api/character.py
@@ -107,16 +107,18 @@ class AliasCharacter(AliasStatBlock):
         self._character.consumables.remove(to_delete)
 
     def create_cc_nx(self, name: str, minVal: str = None, maxVal: str = None, reset: str = None,
-                     dispType: str = None, reset_to: str = None, reset_by: str = None):
+                     dispType: str = None, reset_to: str = None, reset_by: str = None,
+                     title: str = None, desc: str = None):
         """
         Creates a custom counter if one with the given name does not already exist.
         Equivalent to:
 
         >>> if not cc_exists(name):
-        >>>     create_cc(name, minVal, maxVal, reset, dispType, reset_to, reset_by)
+        >>>     create_cc(name, minVal, maxVal, reset, dispType, reset_to, reset_by, title, desc)
         """
         if not self.cc_exists(name):
             new_consumable = player_api.CustomCounter.new(self._character, name, minVal, maxVal, reset, dispType,
+                                                          title=title, desc=desc,
                                                           reset_to=reset_to, reset_by=reset_by)
             self._character.consumables.append(new_consumable)
             self._consumables = None  # reset cache
@@ -133,6 +135,8 @@ class AliasCharacter(AliasStatBlock):
         :param str dispType: Either ``None`` or ``'bubble'``.
         :param str reset_to: The value the counter should reset to. Supports :ref:`cvar-table` parsing.
         :param str reset_by: How much the counter should change by on a reset. Supports dice but not cvars.
+        :param str title: The title of the counter.
+        :param str desc: The description of the counter.
         :rtype: AliasCustomCounter
         :returns: The newly created counter.
         """

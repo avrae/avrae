a2331de5546ec6a541ce84e136b55a1bf8588f51
Andrew Zhu <me@andrew-zhu.com>
2020-12-15 13:14:06 -0800

    command framework

--- cogsmisc/tutorials/__init__.py
+++ cogsmisc/tutorials/__init__.py
@@ -13,6 +13,21 @@ class Tutorials(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.group()
+    async def tutorial(self, ctx):
+        """Shows the current tutorial objective, or lists the available tutorials if one is not active."""
+        pass
+
+    @tutorial.command(name='skip')
+    async def tutorial_skip(self, ctx):
+        """Skips the current objective, and moves on to the next part of the tutorial."""
+        pass
+
+    @tutorial.command(name='end')
+    async def tutorial_end(self, ctx):
+        """Ends the current tutorial."""
+        pass
+
 
 def setup(bot):
     cog = Tutorials(bot)

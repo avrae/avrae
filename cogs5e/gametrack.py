b2fbecfbf1ab8d9629b64c45486c1e89aee9d82f
Andrew Zhu <me@andrew-zhu.com>
2021-02-17 16:17:22 -0800

    AVR-689, AVR-691 refactor death saves to use checkutils
    Resolves #1385
    Resolves #1388

--- cogs5e/gametrack.py
+++ cogs5e/gametrack.py
@@ -16,7 +16,8 @@ from aliasing import helpers
 from cogs5e.models.character import Character, CustomCounter
 from cogs5e.models.embeds import EmbedWithCharacter
 from cogs5e.models.errors import ConsumableException, CounterOutOfBounds, InvalidArgument, NoSelectionElements
-from cogs5e.utils import targetutils
+from cogs5e.models.sheet.base import Skill
+from cogs5e.utils import checkutils, targetutils
 from cogs5e.utils.help_constants import *
 from gamedata.lookuputils import get_spell_choices, select_spell_full
 from utils.argparser import argparse
@@ -243,54 +244,47 @@ class GameTrack(commands.Cog):
         See `!help save`."""
         character: Character = await Character.from_ctx(ctx)
 
+        embed = EmbedWithCharacter(character, name=False)
+
+        args = await helpers.parse_snippets(args, ctx)
+        args = await helpers.parse_with_character(ctx, character, args)
         args = argparse(args)
-        adv = args.adv()
-        b = args.join('b', '+')
-        phrase = args.join('phrase', '\n')
-        dc = args.last('dc', 10, int)
+        checkutils.update_csetting_args(character, args)
+        caster, _, _ = await targetutils.maybe_combat(ctx, character, args)
+        result = checkutils.run_save('death', caster, args, embed)
 
-        if b:
-            save_roll = d20.roll(f"{d20_with_adv(adv)}+{b}")
-        else:
-            save_roll = d20.roll(d20_with_adv(adv))
-
-        embed = discord.Embed()
-        embed.title = args.last('title', '') \
-                          .replace('[name]', character.name) \
-                          .replace('[sname]', 'Death') \
-                      or f'{character.name} makes a Death Save!'
-        embed.colour = character.get_color()
-
+        dc = result.skill_roll_result.dc or 10
         death_phrase = ''
-        if save_roll.crit == d20.CritType.CRIT:
-            character.hp = 1
-        elif save_roll.crit == d20.CritType.FAIL:
-            character.death_saves.fail(2)
-        elif save_roll.total >= dc:
-            character.death_saves.succeed()
-        else:
-            character.death_saves.fail()
 
-        if save_roll.crit == d20.CritType.CRIT:
-            death_phrase = f"{character.name} is UP with 1 HP!"
-        elif character.death_saves.is_dead():
-            death_phrase = f"{character.name} is DEAD!"
-        elif character.death_saves.is_stable():
-            death_phrase = f"{character.name} is STABLE!"
+        for save_roll in result.skill_roll_result.rolls:
+            if save_roll.crit == d20.CritType.CRIT:
+                character.hp = 1
+            elif save_roll.crit == d20.CritType.FAIL:
+                character.death_saves.fail(2)
+            elif save_roll.total >= dc:
+                character.death_saves.succeed()
+            else:
+                character.death_saves.fail()
 
-        await character.commit(ctx)
-        embed.description = save_roll.result + (f'\n*{phrase}*' if phrase else '')
+            if save_roll.crit == d20.CritType.CRIT:
+                death_phrase = f"{character.name} is UP with 1 HP!"
+                break
+            elif character.death_saves.is_dead():
+                death_phrase = f"{character.name} is DEAD!"
+                break
+            elif character.death_saves.is_stable():
+                death_phrase = f"{character.name} is STABLE!"
+                break
+
         if death_phrase:
             embed.set_footer(text=death_phrase)
-        if dc != 10:
-            embed.description = f"**DC {dc}**\n" + embed.description
+        embed.add_field(name="Death Saves", value=str(character.death_saves), inline=False)
 
-        embed.add_field(name="Death Saves", value=str(character.death_saves))
-
-        if args.last('image') is not None:
-            embed.set_thumbnail(url=args.last('image'))
-
+        await character.commit(ctx)
         await ctx.send(embed=embed)
+        await try_delete(ctx.message)
+        if gamelog := self.bot.get_cog('GameLog'):
+            await gamelog.send_save(ctx, character, result.skill_name, result.rolls)
 
     @game_deathsave.command(name='success', aliases=['s', 'save'])
     async def game_deathsave_save(self, ctx):

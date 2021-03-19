39842e8af2a93a80aca9ca7015964a09a2f96428
Andrew Zhu <me@andrew-zhu.com>
2021-03-19 12:55:38 -0700

    chore[gamedata]: add entity-type and type-id to Book class

--- gamedata/book.py
+++ gamedata/book.py
@@ -1,26 +1,29 @@
 from .shared import Sourced
 
-# bitwise source info flags
-FLAG_IS_CR = 1 << 0
-FLAG_IS_UA = 1 << 1
-FLAG_IS_PARTNERED = 1 << 2
-FLAG_IS_NONCORE = 1 << 3
-
 
 class Book(Sourced):
+    entity_type = 'source'
+    type_id = 496802664
+
+    # bitwise source info flags
+    FLAG_IS_CR = 1 << 0
+    FLAG_IS_UA = 1 << 1
+    FLAG_IS_PARTNERED = 1 << 2
+    FLAG_IS_NONCORE = 1 << 3
+
     def __init__(self, name, slug, flags=0, **kwargs):
         """
         :type name: str
         :type slug: str
         :type flags: int
         """
-        super().__init__('book', False, page=None, **kwargs)
+        super().__init__(False, page=None, **kwargs)
         self.name = name
         self.slug = slug
-        self.is_cr = flags & FLAG_IS_CR
-        self.is_ua = flags & FLAG_IS_UA
-        self.is_partnered = flags & FLAG_IS_PARTNERED
-        self.is_noncore = flags & FLAG_IS_NONCORE
+        self.is_cr = flags & Book.FLAG_IS_CR
+        self.is_ua = flags & Book.FLAG_IS_UA
+        self.is_partnered = flags & Book.FLAG_IS_PARTNERED
+        self.is_noncore = flags & Book.FLAG_IS_NONCORE
 
     @classmethod
     def from_data(cls, d):

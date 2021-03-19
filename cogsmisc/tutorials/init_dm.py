8eb3d21588669f4040378268ae5fa7b0986577d0
Andrew Zhu <me@andrew-zhu.com>
2021-03-19 13:26:49 -0700

    chore[tutorials]: remove magic string

--- cogsmisc/tutorials/init_dm.py
+++ cogsmisc/tutorials/init_dm.py
@@ -1,6 +1,7 @@
 import asyncio
 
 from cogs5e.models.initiative import CombatNotFound, MonsterCombatant
+from gamedata import Monster
 from gamedata.compendium import compendium
 from utils.functions import confirm
 from .errors import PrerequisiteFailed
@@ -532,7 +533,7 @@ class DMInitiative(Tutorial):
 
 
 async def add_orkira(ctx, combat):
-    priest = compendium.lookup_entity('monster', 16985)
+    priest = compendium.lookup_entity(Monster.entity_type, 16985)
     orkira = MonsterCombatant.from_monster(
         monster=priest,
         ctx=ctx,

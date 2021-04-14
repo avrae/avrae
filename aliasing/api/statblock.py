eb11cd4395bbe918fbaf584804645a70f1a832b6
Dice <64364022+driesceuppens@users.noreply.github.com>
2021-04-14 22:06:01 +0200

    Adding support for <, > and == in aliasskill.
    ### Summary
    Adding support for the <, > and == operators in aliasSkill
    
    ### Checklist
    <!-- Put an "x" inside the braces to tick checkboxes, e.g. [x]. -->
    #### PR Type
    <!-- If the PR closes an issue, mention the issue at the top of the PR with "Resolves #X". -->
    - [x ] This PR is a code change that implements a feature request.
    - [ ] This PR fixes an issue.
    - [ ] This PR adds a new feature that is not an open feature request.
    - [ ] This PR is not a code change (e.g. documentation, README, ...)
    #### Other
    - [ ] This PR is a breaking change (e.g. methods or parameters removed/renamed)
    - [ ] If code changes were made then they have been tested.
    - [ ] I have updated the documentation to reflect the changes.

--- aliasing/api/statblock.py
+++ aliasing/api/statblock.py
@@ -475,6 +475,16 @@ class AliasSkill:
     def __repr__(self):
         return f"<{self.__class__.__name__} value={self.value!r} prof={self.prof!r} bonus={self.bonus!r} " \
                f"adv={self.adv!r}>"
+    
+    def __gt__(self, other):
+        return self.value > other.value
+    
+    def __lt__(self, other):
+        return self.value < other.value
+    
+    def __eq__(self, other):
+        return self.value == other.value
+    
 
 
 class AliasSkills:

6061ab0a732d991c5717673740209dad74ab3016
Dice <64364022+driesceuppens@users.noreply.github.com>
2021-04-24 00:22:28 +0200

    Update and rename statblock_test.py to functions_test.py

---
+++ tests/aliasing/functions_test.py
@@ -0,0 +1,31 @@
+from aliasing.api.functions import randint, randchoice
+import pytest
+
+
+# TODO: AliasStatBlock tests
+# TODO: AliasBaseStats tests
+# TODO: AliasLevels tests
+# TODO: AliasAttack tests
+# TODO: AliasSkill tests
+def test_randint_and_randchoice_tests():
+    for i in range(100):
+        r1 = randint(100)
+        r2 = randint(50, 100)
+        r3 = randint(10, 100, 2)
+        seq = [1, 2, 3, 4, 5]
+        c1 = randchoice(seq)
+        
+        assert r1 < 100
+        
+        assert r2 >=50
+        assert r2 < 100
+        
+        assert r3 <= 10
+        assert r3 % 2 == 0
+        assert r3 < 100
+        
+        assert c1 in seq
+# TODO: AliasSaves tests
+# TODO: AliasResistances Tests
+# TODO: AliasSpellbook Tests
+# TODO: AliasSpellbookSpell tests

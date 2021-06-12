a1667284f2377a38f89c877ab191f65b436a2eb8
Dice <64364022+driesceuppens@users.noreply.github.com>
2021-06-12 23:07:33 +0200

    fix these tests too

--- tests/cogmisc/costumisation_test.py
+++ tests/cogmisc/costumisation_test.py
@@ -23,13 +23,11 @@ async def test_snippet_before_edit(avrae, dhttp):
     await dhttp.receive_message("Warning: making a snippet named `adv` will prevent you from using the built-in `adv` argument in Avrae commands.\nAre you sure you want to make this snippet?(Y/N)", regex = False)
     avrae.message('no')
     await dhttp.receive_message('Ok, cancelling.', regex = False)
-    await dhttp.recieve_message('The snippet was not created.', regex = False)
 
     avrae.message('!snippet str adv')
     await dhttp.receive_message("Warning: making a snippet named `str` will prevent you from using the built-in `str` argument in Avrae commands.\nAre you sure you want to make this snippet?(Y/N)", regex = False)
     avrae.message('no')
     await dhttp.receive_message('Ok, cancelling.', regex = False)
-    await dhttp.recieve_message('The snippet was not created.', regex = False)
 
     avrae.message('!snippet 10 adv')
     await dhttp.receive_message('You can not use any valid dice strings as the name of a snippet.', regex = False)

b5eac34fb8da2f53ca63d5bb3660d7f1db6ee928
Dice <64364022+driesceuppens@users.noreply.github.com>
2021-06-12 22:07:42 +0200

    maybe right now?
    Also fixed the name of the snippet in the second to last test

--- tests/cogmisc/costumisation_test.py
+++ tests/cogmisc/costumisation_test.py
@@ -9,27 +9,27 @@ async def test_snippet_before_edit(avrae, dhttp):
     dhttp.clear()
     
     avrae.message('!snippet test adv')
-    await dhttp.receive_message(rf"Snippet `test` added.\n```py\n!snippet test adv\n```")
+    await dhttp.receive_message(rf"Snippet `test` added.\n```py\n!snippet test adv\n```", regex = False)
 
     avrae.message('!snippet 2d6 adv')
-    await dhttp.receive_message('You can not use any valid dice strings as the name of a snippet.')
+    await dhttp.receive_message('You can not use any valid dice strings as the name of a snippet.', regex = False)
 
     avrae.message('!snippet adv adv')
-    await dhttp.receive_message("Warning: making a snippet named `adv` will prevent you from using the built-in `adv` argument in Avrae commands.\nAre you sure you want to make this snippet?(Y/N)")
+    await dhttp.receive_message("Warning: making a snippet named `adv` will prevent you from using the built-in `adv` argument in Avrae commands.\nAre you sure you want to make this snippet?(Y/N)", regex = False)
     avrae.message('yes')
-    await dhttp.receive_message(rf"Snippet `adv` added.\n```py\n!snippet adv adv\n```")
+    await dhttp.receive_message(rf"Snippet `adv` added.\n```py\n!snippet adv adv\n```", regex = False)
 
     avrae.message('!snippet adv adv')
-    await dhttp.receive_message("Warning: making a snippet named `adv` will prevent you from using the built-in `adv` argument in Avrae commands.\nAre you sure you want to make this snippet?(Y/N)")
+    await dhttp.receive_message("Warning: making a snippet named `adv` will prevent you from using the built-in `adv` argument in Avrae commands.\nAre you sure you want to make this snippet?(Y/N)", regex = False)
     avrae.message('no')
-    await dhttp.receive_message('Ok, cancelling.')
+    await dhttp.receive_message('Ok, cancelling.', regex = False)
 
     avrae.message('!snippet str adv')
-    await dhttp.receive_message("Warning: making a snippet named `adv` will prevent you from using the built-in `adv` argument in Avrae commands.\nAre you sure you want to make this snippet?(Y/N)")
+    await dhttp.receive_message("Warning: making a snippet named `str` will prevent you from using the built-in `str` argument in Avrae commands.\nAre you sure you want to make this snippet?(Y/N)", regex = False)
     avrae.message('no')
-    await dhttp.receive_message('Ok, cancelling.')
+    await dhttp.receive_message('Ok, cancelling.', regex = False)
 
     avrae.message('!snippet 10 adv')
-    await dhttp.receive_message('You can not use any valid dice strings as the name of a snippet.')
+    await dhttp.receive_message('You can not use any valid dice strings as the name of a snippet.', regex = False)
 
 

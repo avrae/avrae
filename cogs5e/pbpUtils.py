ece8e06149ce52a008e6dc16e605108f6f226752
Storyteller Mahkasad <storytellermahkasad@gmail.com>
2021-07-15 10:22:31 -0600

    Update docs for techo to show timeout range

--- cogs5e/pbpUtils.py
+++ cogs5e/pbpUtils.py
@@ -26,7 +26,8 @@ class PBPUtils(commands.Cog):
 
     @commands.command()
     async def techo(self, ctx, seconds: int, *, msg):
-        """Echos a message, and deletes it after a few seconds."""
+        """Echos a message, and deletes it after a number of seconds.
+        Timeout: 0-600 seconds. Default: 0"""
         await try_delete(ctx.message)
 
         seconds = min(max(0, seconds), 600)

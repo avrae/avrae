9813b623476bc9a33852dd13d7abb7877a01cb16
Dice <64364022+driesceuppens@users.noreply.github.com>
2021-07-19 18:50:37 +0200

    Facepalm

--- tests/aliasing/evaluators_test.py
+++ tests/aliasing/evaluators_test.py
@@ -19,6 +19,9 @@ async def test_yaml_loading(avrae, dhttp):
 
 
 async def yaml_test_types(avrae, dhttp):
+    yaml_string = """key1:\n nested_key1: 06-08-2012\n nested_key2: value2\n nested_key3: value3\nthing1: """ \
+                  """one\nthing2: two\nthing3:\n - 1\n - 2\n - 3 """
+
     avrae.message(f'''!test <drac2>\nyaml_string = """{yaml_string}"""\nloaded = load_yaml(yaml_string)\nreturn '''
                   '''typeof(loaded)\n</drac2>''')
     await dhttp.receive_message(r'.*: SafeDict', regex=True)
@@ -37,6 +40,8 @@ async def yaml_test_types(avrae, dhttp):
 
 
 async def yaml_test_errors(avrae, dhttp):
+    yaml_string = """key1:\n nested_key1: 06-08-2012\n nested_key2: value2\n nested_key3: value3\nthing1: """ \
+                  """one\nthing2: two\nthing3:\n - 1\n - 2\n - 3 """
     avrae.message('!test {{load_yaml(4)}}')
     await dhttp.receive_message("Error evaluating expression: 'int' object has no attribute 'read'", regex=False)
 
@@ -46,6 +51,8 @@ async def yaml_test_errors(avrae, dhttp):
 
 
 async def yaml_test_dumping(avrae, dhttp):
+    yaml_string = """key1:\n nested_key1: 06-08-2012\n nested_key2: value2\n nested_key3: value3\nthing1: """ \
+                  """one\nthing2: two\nthing3:\n - 1\n - 2\n - 3 """
     avrae.message("""!test <drac2>\ndata = {"name": "Dice", "age": "old", "languages": 3, "drinks": ["beer","""
                   """ "wine", "apple juice"]}\nreturn dump_yaml(data)\n</drac2>""")
     await dhttp.recieve_message("age: old\ndrinks:\n - beer\n - wine\n - apple juice\nlanguages: 3\nname: Dice",

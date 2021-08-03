486e0e01f3757d20164be520f9d90f9a6f785504
Andrew Zhu <me@andrew-zhu.com>
2021-08-03 12:33:04 -0700

    fix: circular import when importing gamedata

--- gamedata/spell.py
+++ gamedata/spell.py
@@ -5,12 +5,12 @@ from collections import namedtuple
 import discord
 
 import gamedata.lookuputils
-from cogs5e.models import initiative
 from cogs5e.models.embeds import EmbedWithAuthor, add_fields_from_args
 from cogs5e.models.errors import AvraeException, InvalidArgument
+from cogs5e.models.initiative.effect import Effect
 from cogs5e.models.initiative.types import BaseCombatant
 from utils.constants import STAT_ABBREVIATIONS
-from utils.functions import smart_trim, verbose_stat, maybe_http_url
+from utils.functions import maybe_http_url, smart_trim, verbose_stat
 from .mixins import AutomatibleMixin, DescribableMixin
 from .shared import Sourced
 
@@ -221,7 +221,7 @@ class Spell(AutomatibleMixin, DescribableMixin, Sourced):
             embed.title = title.replace('[name]', caster.name) \
                 .replace('[aname]', self.name) \
                 .replace('[sname]', self.name) \
-                .replace('[verb]', 'casts')   # #1514, [aname] is action name now, #1587, add verb to action/cast
+                .replace('[verb]', 'casts')  # #1514, [aname] is action name now, #1587, add verb to action/cast
         else:
             embed.title = f"{caster.get_title_name()} casts {self.name}{stat_override}!"
         if targets is None:
@@ -233,7 +233,7 @@ class Spell(AutomatibleMixin, DescribableMixin, Sourced):
         conc_effect = None
         if all((self.concentration, isinstance(caster, BaseCombatant), combat, not noconc)):
             duration = args.last('dur', self.get_combat_duration(), int)
-            conc_effect = initiative.Effect.new(combat, caster, self.name, duration, "", True)
+            conc_effect = Effect.new(combat, caster, self.name, duration, "", True)
             effect_result = caster.add_effect(conc_effect)
             conc_conflict = effect_result['conc_conflict']
 

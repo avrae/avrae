4661a4ab6a494cffbcbd068ec6e705c2d842f804
Andrew Zhu <me@andrew-zhu.com>
2021-09-08 13:53:14 -0700

    build: fix some tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -97,10 +97,10 @@ def message_content_check(request: Request, content: str = None, *, regex: bool 
         else:
             assert request.data.get('content') == content
     if embed:
-        embed_data = request.data.get('embed')
-        assert embed_data is not None
-        embed_assertations(discord.Embed.from_dict(embed_data))
-        compare_embeds(embed_data, embed.to_dict(), regex=regex)
+        embed_data = request.data.get('embeds')
+        assert embed_data is not None and embed_data
+        embed_assertations(discord.Embed.from_dict(embed_data[0]))
+        compare_embeds(embed_data[0], embed.to_dict(), regex=regex)
     return match
 
 
@@ -254,22 +254,30 @@ def message(self, message_content, as_owner=False, dm=False):
 
     log.info(f"Sending message {message_content}")
     # pretend we just received a message in our testing channel
-    self._connection.parse_message_create({
-        "attachments": [],
-        "tts": False,
-        "embeds": [],
-        "timestamp": "2017-07-11T17:27:07.299000+00:00",
-        "mention_everyone": False,
-        "id": MESSAGE_ID,
-        "pinned": False,
-        "edited_timestamp": None,
-        "author": DEFAULT_USER if not as_owner else OWNER_USER,
-        "mention_roles": [],
-        "content": message_content,
-        "channel_id": str(TEST_CHANNEL_ID) if not dm else str(TEST_DMCHANNEL_ID),
-        "mentions": [],
-        "type": 0
-    })
+    data = {
+        'type': 0,
+        'tts': False,
+        'timestamp': '2021-09-08T20:33:57.337000+00:00',
+        'referenced_message': None,
+        'pinned': False,
+        'nonce': 'blah',
+        'mentions': [],
+        'mention_roles': [],
+        'mention_everyone': False,
+        'id': MESSAGE_ID,
+        'flags': 0,
+        'embeds': [],
+        'edited_timestamp': None,
+        'content': message_content,
+        'components': [],
+        'channel_id': str(TEST_CHANNEL_ID) if not dm else str(TEST_DMCHANNEL_ID),
+        'author': DEFAULT_USER if not as_owner else OWNER_USER,
+        'attachments': []
+    }
+    if not dm:
+        data['guild_id'] = TEST_GUILD_ID
+
+    self._connection.parse_message_create(data)
     return MESSAGE_ID
 
 
@@ -310,7 +318,7 @@ async def avrae(dhttp):
     # noinspection PyProtectedMember
     bot._connection.parse_guild_create(DUMMY_GUILD_CREATE)
     # noinspection PyProtectedMember
-    bot._connection.parse_channel_create(DUMMY_DMCHANNEL_CREATE)
+    bot._connection.add_dm_channel(DUMMY_DMCHANNEL_CREATE)
 
     log.info("Ready for testing")
     yield bot

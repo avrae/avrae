f56da221bd2473d36509e1723a8f8de00a18fc62
Dice <64364022+driesceuppens@users.noreply.github.com>
2021-09-25 00:00:30 +0200

    allow for serving aliases

--- tests/cogsmisc/customization_test.py
+++ tests/cogsmisc/customization_test.py
@@ -6,83 +6,107 @@ pytestmark = pytest.mark.asyncio
 
 async def test_snippet_before_edit(avrae, dhttp):
     dhttp.clear()
-
+    
     # Snippet tests
     avrae.message("!snippet test adv")
     await dhttp.receive_message("Snippet `test` added.```py\n!snippet test adv\n```", regex=False)
-
+    
     avrae.message("!snippet 2d6 adv")
-    await dhttp.receive_message("**Warning:** Creating a snippet named `2d6` might cause hidden problems "
-                                "if you try to use the same roll in other commands.\nAre you sure you want to "
-                                "create this snippet? (Reply with yes/no)", regex=False)
+    await dhttp.receive_message(
+            "**Warning:** Creating a snippet named `2d6` might cause hidden problems "
+            "if you try to use the same roll in other commands.\nAre you sure you want to "
+            "create this snippet? (Reply with yes/no)", regex=False
+    )
     avrae.message("no")
     await dhttp.receive_message("Ok, cancelling.", regex=False)
-
+    
     avrae.message("!snippet adv adv")
-    await dhttp.receive_message("**Warning:** Creating a snippet named `adv` will prevent you from using "
-                                "the built-in `adv` argument in Avrae commands.\nAre you sure you want to "
-                                "create this snippet? (Reply with yes/no)", regex=False)
+    await dhttp.receive_message(
+            "**Warning:** Creating a snippet named `adv` will prevent you from using "
+            "the built-in `adv` argument in Avrae commands.\nAre you sure you want to "
+            "create this snippet? (Reply with yes/no)", regex=False
+    )
     avrae.message("yes")
     await dhttp.receive_message("Snippet `adv` added.```py\n!snippet adv adv\n```", regex=False)
-
+    
     avrae.message("!snippet adv adv")
-    await dhttp.receive_message("**Warning:** Creating a snippet named `adv` will prevent you from using "
-                                "the built-in `adv` argument in Avrae commands.\nAre you sure you want to "
-                                "create this snippet? (Reply with yes/no)", regex=False)
+    await dhttp.receive_message(
+            "**Warning:** Creating a snippet named `adv` will prevent you from using "
+            "the built-in `adv` argument in Avrae commands.\nAre you sure you want to "
+            "create this snippet? (Reply with yes/no)", regex=False
+    )
     avrae.message("no")
     await dhttp.receive_message("Ok, cancelling.", regex=False)
-
+    
     avrae.message("!snippet str adv")
-    await dhttp.receive_message("**Warning:** Creating a snippet named `str` will prevent you from using "
-                                "the built-in `str` argument in Avrae commands.\nAre you sure you want to "
-                                "create this snippet? (Reply with yes/no)", regex=False)
+    await dhttp.receive_message(
+            "**Warning:** Creating a snippet named `str` will prevent you from using "
+            "the built-in `str` argument in Avrae commands.\nAre you sure you want to "
+            "create this snippet? (Reply with yes/no)", regex=False
+    )
     avrae.message("no")
     await dhttp.receive_message("Ok, cancelling.", regex=False)
-
+    
     avrae.message("!snippet 10 adv")
-    await dhttp.receive_message("**Warning:** Creating a snippet named `10` might cause hidden problems if "
-                                "you try to use the same roll in other commands.\nAre you sure you want to "
-                                "create this snippet? (Reply with yes/no)", regex=False)
+    await dhttp.receive_message(
+            "**Warning:** Creating a snippet named `10` might cause hidden problems if "
+            "you try to use the same roll in other commands.\nAre you sure you want to "
+            "create this snippet? (Reply with yes/no)", regex=False
+    )
     avrae.message("no")
     await dhttp.receive_message("Ok, cancelling.", regex=False)
-
+    
     avrae.message("!snippet remove test")
     await dhttp.receive_message("Snippet test removed.", regex=False)
-
+    
     avrae.message("!snippet remove adv")
     await dhttp.receive_message("Snippet adv removed.", regex=False)
-
+    
     avrae.message("!serversnippet adv adv", as_owner=True)
-    await dhttp.receive_message("**Warning:** Creating a snippet named `adv` will prevent you from using "
-                                "the built-in `adv` argument in Avrae commands.\nAre you sure you want to "
-                                "create this snippet? (Reply with yes/no)", regex=False)
+    await dhttp.receive_message(
+            "**Warning:** Creating a snippet named `adv` will prevent you from using "
+            "the built-in `adv` argument in Avrae commands.\nAre you sure you want to "
+            "create this snippet? (Reply with yes/no)", regex=False
+    )
     avrae.message("yes", as_owner=True)
     await dhttp.receive_message("Server snippet `adv` added.```py\n!snippet adv adv\n```", regex=False)
-
+    
     # alias tests
     avrae.message("!alias tester echo test")
     await dhttp.receive_message("Alias `tester` added.```py\n!alias tester echo test\n```", regex=False)
-
+    
     avrae.message("!alias test echo test")
     await dhttp.receive_message("`test` is already a builtin command. Try another name.", regex=False)
-
+    
     avrae.message("!servalias tester echo test", as_owner=True)
     await dhttp.receive_message("Server alias `tester` added.```py\n!alias tester echo test\n```", regex=False)
-
+    
     avrae.message("!servalias test echo test", as_owner=True)
     await dhttp.receive_message("`test` is already a builtin command. Try another name.", regex=False)
-
+    
     # testing the bugfix for renaming
     avrae.message("!snippet do adv")
     await dhttp.receive_message("Snippet `do` added.```py\n!snippet do adv\n```", regex=False)
     avrae.message("!snippet rename do adv")
-    await dhttp.receive_message("**Warning:** Creating a snippet named `adv` will prevent you from using "
-                                "the built-in `adv` argument in Avrae commands.\nAre you sure you want to "
-                                "create this snippet? (Reply with yes/no)", regex=False)
+    await dhttp.receive_message(
+            "**Warning:** Creating a snippet named `adv` will prevent you from using "
+            "the built-in `adv` argument in Avrae commands.\nAre you sure you want to "
+            "create this snippet? (Reply with yes/no)", regex=False
+    )
     avrae.message("yes")
     await dhttp.receive_message("Okay, renamed the snippet do to adv.", regex=False)
-
+    
     avrae.message("!alias tester echo not a test")
     await dhttp.receive_message("Alias `tester` added.```py\n!alias tester echo not a test\n```", regex=False)
     avrae.message("!alias rename tester test")
     await dhttp.receive_message("`test` is already a builtin command. Try another name.", regex=False)
+
+
+async def servering_aliases(avrae, dhttp):
+    dhttp.clear()
+    avrae.message("!alias foobar echo foobar")
+    
+    dhttp.drain()
+    avrae.message("!alias serve foobar")
+    await dhttp.receive_message(r'Server alias `foobar` added\.\n```py\n.foobar echo foobar\n?```', regex=True)
+    

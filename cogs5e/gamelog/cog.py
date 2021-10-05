3ea930cec1661c94b65e5d63e77e4cc8f33e29dc
Andrew Zhu <me@andrew-zhu.com>
2021-10-05 13:19:39 -0700

    Revert "chore(gamelog): resource sync helpdoc"
    This reverts commit c79ce95920a25f1a5c0d66ffe05e93c731005b52.

--- cogs5e/gamelog/cog.py
+++ cogs5e/gamelog/cog.py
@@ -41,15 +41,12 @@ class GameLog(commands.Cog):
     @checks.feature_flag('command.campaign.enabled', use_ddb_user=True)
     async def campaign(self, ctx, campaign_link=None):
         """
-        Links a D&D Beyond campaign to this channel to synchronize players' rolls and character information.
-
-        • See rolls from your D&D Beyond character sheet and send rolls to the Game Log!
-        • Synchronize your character's hit points, spell slots, custom counters, and more!
+        Links a D&D Beyond campaign to this channel, displaying rolls made on players' character sheets in real time.
 
         You must be the DM of the campaign to link it to a channel.
 
-        Not seeing a player's rolls? Link their D&D Beyond and Discord accounts [here](https://www.dndbeyond.com/account), and check with the `!ddb` command.
-        """  # noqa: E501
+        Not seeing a player's rolls? Link their D&D Beyond and Discord accounts [here](https://www.dndbeyond.com/account), and check with the `!ddb` command!
+        """
         if campaign_link is None:
             return await self.campaign_list(ctx)
 

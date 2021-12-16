af6c8b474347af814570deb51c640bddae3339ca
Andrew Zhu <me@andrew-zhu.com>
2021-12-16 14:08:21 -0800

    chore(dice): improve adv search

--- cogs5e/dice/utils.py
+++ cogs5e/dice/utils.py
@@ -2,18 +2,20 @@ import re
 
 import d20
 
+ADV_WORD_RE = re.compile(r'(?:^|\s+)(adv|dis)(?:\s+|$)')
 
-def string_search_adv(dice_str):
+
+def string_search_adv(dice_str: str):
     """
     Given a dice string, returns whether the word adv or dis was found within it, and the string with the word removed.
 
     >>> string_search_adv("1d20 adv")
-    ("1d20", d20.AdvType.ADV)
+    ("1d20 ", d20.AdvType.ADV)
     >>> string_search_adv("1d20")
     ("1d20", d20.AdvType.NONE)
     """
     adv = d20.AdvType.NONE
-    if re.search(r'(^|\s+)(adv|dis)(\s+|$)', dice_str) is not None:
-        adv = d20.AdvType.ADV if re.search(r'(^|\s+)adv(\s+|$)', dice_str) is not None else d20.AdvType.DIS
-        dice_str = re.sub(r'(adv|dis)(\s+|$)', '', dice_str)
+    if (match := ADV_WORD_RE.search(dice_str)) is not None:
+        adv = d20.AdvType.ADV if match.group(1) == 'adv' else d20.AdvType.DIS
+        return dice_str[:match.start(1)] + dice_str[match.end():], adv
     return dice_str, adv

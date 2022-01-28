dabe7c401fe73ac7ec375770290f49b27b694099
Andrew Zhu <me@andrew-zhu.com>
2022-01-28 14:15:04 -0800

    build: fix the build for new init order

--- tests/cogs5e/initTracker_test.py
+++ tests/cogs5e/initTracker_test.py
@@ -16,9 +16,9 @@ class TestInitiativeSimple:
         avrae.message("!init begin")
         await dhttp.receive_delete()
         await dhttp.receive_message("```Awaiting combatants...```")
+        await dhttp.receive_pin()
         await dhttp.receive_edit("```md\nCurrent initiative: 0 (round 0)\n===============================\n```",
                                  regex=False)
-        await dhttp.receive_pin()
         await dhttp.receive_message("Everyone roll for initiative!\nIf you have a character set up with SheetManager: "
                                     "`!init join`\nIf it's a 5e monster: `!init madd <monster name>`\nOtherwise: "
                                     "`!init add <modifier> <name>`", regex=False)

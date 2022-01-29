fc86ab26e42e6fafe08dbbd81852846ba009d42a
Kyle Sletten <kyle.sletten@faithlife.com>
2022-01-29 11:09:58 -0700

    Move reset_slots to correct class

--- gamedata/monster.py
+++ gamedata/monster.py
@@ -390,11 +390,6 @@ class MonsterSpellbook(Spellbook):
         })
         return d
 
-    def reset_slots(self):
-        super().reset_slots()
-        for spell, max in self.daily_max.items():
-            self.daily[spell] = max
-
     # ===== utils =====
     def slots_str(self, level: int = None):
         if level is not None:
@@ -454,3 +449,8 @@ class MonsterCastableSpellbook(MonsterSpellbook):
                 raise CounterOutOfBounds(f"You do not have any remaining casts of {spell.name}.")
         else:
             self.use_slot(level, pact=pact)
+
+    def reset_slots(self):
+        super().reset_slots()
+        for spell, max in self.daily_max.items():
+            self.daily[spell] = max

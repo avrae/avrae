d4dcf6411d20489586b5a24d5da0873e967eeeec
Sean Stoves <sstoves@fandom.com>
2022-03-04 17:40:53 -0500

    Updating Total Value message

--- cogs5e/utils/gameutils.py
+++ cogs5e/utils/gameutils.py
@@ -41,7 +41,7 @@ async def send_current_coin(ctx, character, coin="All"):
     cp_display_embed = EmbedWithCharacter(character)
     cp_display_embed.set_footer(text=f"For help managing your coins, use {ctx.prefix}game coinpurse")
     cp_display_embed.set_thumbnail(url="https://www.dndbeyond.com/attachments/thumbnails/3/929/650/358/scag01-04.png")
-    cp_display_embed.add_field(name="Total Value in Purse",
+    cp_display_embed.add_field(name="Total Value",
                                value=character.coinpurse.str_styled('compact'),
                                inline=False)
     if coin == "All":

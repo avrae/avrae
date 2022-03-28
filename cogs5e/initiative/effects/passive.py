5f329e1185781d78ef315a72357717be6875df91
Andrew Zhu <me@andrew-zhu.com>
2022-03-28 09:02:14 -0700

    refactor: use descriptors for passive effects

--- cogs5e/initiative/effects/passive.py
+++ cogs5e/initiative/effects/passive.py
@@ -1,4 +1,4 @@
-from typing import Iterable, List, Optional, Set, Tuple
+from typing import Callable, Iterable, List, Optional, Set, Tuple, Type, TypeVar, Union
 
 from cogs5e.models.errors import InvalidArgument
 from cogs5e.models.sheet.resistance import Resistance
@@ -7,9 +7,65 @@ from utils.constants import STAT_ABBREVIATIONS
 from utils.enums import AdvantageType
 from utils.functions import verbose_stat
 
+_OwnerT = TypeVar("_OwnerT")
+_DT = TypeVar("_DT")
+_DefaultT = TypeVar("_DefaultT")
+_SerializedT = TypeVar("_SerializedT")
 
+
+def noop(x):
+    return x
+
+
+class _PassiveEffect:
+    """A descriptor to reduce code repetition when defining passive effects."""
+
+    def __init__(
+        self,
+        datatype: Type[_DT],
+        stringifier: Callable[[_DT], str],
+        deserializer: Callable[[_SerializedT], _DT] = noop,
+        serializer: Callable[[_DT], _SerializedT] = noop,
+        default: _DefaultT = None,
+    ):
+        self.datatype = datatype
+        self.stringifier = stringifier
+        self.deserializer = deserializer
+        self.serializer = serializer
+        self.default = default
+
+    def __set_name__(self, owner: Type[_OwnerT], name: str):
+        if not hasattr(owner, "__effect_attrs"):
+            owner.__effect_attrs = set()
+        if not hasattr(owner, "__effect_deserializers"):
+            owner.__effect_deserializers = {}
+        if not hasattr(owner, "__effect_serializers"):
+            owner.__effect_serializers = {}
+        if not hasattr(owner, "__effect_stringifiers"):
+            owner.__effect_stringifiers = {}
+
+        owner.__effect_attrs.add(name)
+        owner.__effect_deserializers[name] = self.deserializer
+        owner.__effect_serializers[name] = self.serializer
+        owner.__effect_stringifiers[name] = self.stringifier
+        self.private_name = "_" + name
+
+    def __get__(self, obj: _OwnerT, objtype: Type[_OwnerT] = None) -> Union[_DT, _DefaultT]:
+        value = getattr(obj, self.private_name, self.default)
+        return value
+
+    def __set__(self, obj: _OwnerT, value: _DT):
+        if value is None:
+            value = self.default
+        if not isinstance(value, self.datatype):
+            raise ValueError(f"{self.private_name} must be {self.datatype.__name__} but got {type(value).__name__}")
+        setattr(obj, self.private_name, value)
+
+
+# ==== impl ====
+# ---- stringifiers ----
 def _abstract_str_attr(attr_title: str):
-    def impl(_, value):
+    def impl(value):
         return f"{attr_title}: {value}"
 
     return impl
@@ -20,140 +76,131 @@ def _abstract_bool_attr(value_if_present: str):
 
 
 def _abstract_list_attr(attr_title: str):
-    def impl(_, value: Iterable):
+    def impl(value: Iterable):
         values = ", ".join(str(v) for v in value)
         return f"{attr_title}: {values}"
 
     return impl
 
 
+def _str_attack_advantage(value: AdvantageType):
+    if value == AdvantageType.ADV:
+        return "Attack Advantage"
+    elif value == AdvantageType.DIS:
+        return "Attack Disadvantage"
+    elif value == AdvantageType.ELVEN:
+        return "Attack Advantage: Elven Accuracy"
+
+
+def _str_save_adv(value: Set[str]):
+    if value.issuperset(STAT_ABBREVIATIONS):
+        return "Save Advantage: All"
+    saves = ", ".join(verbose_stat(s) for s in value)
+    return f"Save Advantage: {saves}"
+
+
+def _str_save_dis(value: Set[str]):
+    if value.issuperset(STAT_ABBREVIATIONS):
+        return "Save Disadvantage: All"
+    saves = ", ".join(verbose_stat(s) for s in value)
+    return f"Save Disadvantage: {saves}"
+
+
+# ---- main class ----
 class InitPassiveEffect:
     """
     Represents all the passive effects granted by an Initiative effect.
-    If adding new passive effects, you need to make 4 updates: __slots__, __init__, from_args, and _str_attr
+    If adding new passive effects, you need to make 3 updates: in the classvars, from_args, and _str_attr
     (and possibly to/from dict depending on your serialization logic).
     """
 
-    # it might be worth using some kind of descriptor here, because this is a lot of repetition
-    __slots__ = (
-        "attack_advantage",
-        "to_hit_bonus",
-        "damage_bonus",
-        "magical_damage",
-        "silvered_damage",
-        "resistances",
-        "immunities",
-        "vulnerabilities",
-        "ignored_resistances",
-        "ac_value",
-        "ac_bonus",
-        "max_hp_value",
-        "max_hp_bonus",
-        "save_bonus",
-        "save_adv",
-        "save_dis",
-        "check_bonus",
-    )
+    __effect_attrs = set()
+    __effect_stringifiers = {}
+    __effect_deserializers = {}
+    __effect_serializers = {}
 
-    def __init__(
-        self,
-        # attacks
-        attack_advantage: Optional[AdvantageType] = None,
-        to_hit_bonus: Optional[str] = None,
-        damage_bonus: Optional[str] = None,
-        magical_damage: bool = False,
-        silvered_damage: bool = False,
-        # resists,
-        resistances: List[Resistance] = None,
-        immunities: List[Resistance] = None,
-        vulnerabilities: List[Resistance] = None,
-        ignored_resistances: List[Resistance] = None,
-        # general,
-        ac_value: Optional[int] = None,
-        ac_bonus: Optional[int] = None,
-        max_hp_value: Optional[int] = None,
-        max_hp_bonus: Optional[int] = None,
-        save_bonus: Optional[str] = None,
-        save_adv: Set[str] = None,
-        save_dis: Set[str] = None,
-        check_bonus: Optional[str] = None,
-    ):
-        if resistances is None:
-            resistances = []
-        if immunities is None:
-            immunities = []
-        if vulnerabilities is None:
-            vulnerabilities = []
-        if ignored_resistances is None:
-            ignored_resistances = []
-        if save_adv is None:
-            save_adv = set()
-        if save_dis is None:
-            save_dis = set()
-        self.attack_advantage = attack_advantage
-        self.to_hit_bonus = to_hit_bonus
-        self.damage_bonus = damage_bonus
-        self.magical_damage = magical_damage
-        self.silvered_damage = silvered_damage
-        self.resistances = resistances
-        self.immunities = immunities
-        self.vulnerabilities = vulnerabilities
-        self.ignored_resistances = ignored_resistances
-        self.ac_value = ac_value
-        self.ac_bonus = ac_bonus
-        self.max_hp_value = max_hp_value
-        self.max_hp_bonus = max_hp_bonus
-        self.save_bonus = save_bonus
-        self.save_adv = save_adv
-        self.save_dis = save_dis
-        self.check_bonus = check_bonus
+    attack_advantage = _PassiveEffect(
+        AdvantageType,
+        stringifier=_str_attack_advantage,
+        deserializer=lambda data: AdvantageType(data),
+        serializer=lambda data: data.value,
+    )
+    to_hit_bonus = _PassiveEffect(str, stringifier=_abstract_str_attr("Attack Bonus"))
+    damage_bonus = _PassiveEffect(str, stringifier=_abstract_str_attr("Damage Bonus"))
+    magical_damage = _PassiveEffect(bool, stringifier=_abstract_bool_attr("Magical Damage"))
+    silvered_damage = _PassiveEffect(bool, stringifier=_abstract_bool_attr("Silvered Damage"))
+    resistances = _PassiveEffect(
+        List[Resistance],
+        default=[],
+        stringifier=_abstract_list_attr("Resistance"),
+        deserializer=lambda data: [Resistance.from_dict(r) for r in data],
+        serializer=lambda data: [r.to_dict() for r in data],
+    )
+    immunities = _PassiveEffect(
+        List[Resistance],
+        default=[],
+        stringifier=_abstract_list_attr("Immunity"),
+        deserializer=lambda data: [Resistance.from_dict(r) for r in data],
+        serializer=lambda data: [r.to_dict() for r in data],
+    )
+    vulnerabilities = _PassiveEffect(
+        List[Resistance],
+        default=[],
+        stringifier=_abstract_list_attr("Vulnerability"),
+        deserializer=lambda data: [Resistance.from_dict(r) for r in data],
+        serializer=lambda data: [r.to_dict() for r in data],
+    )
+    ignored_resistances = _PassiveEffect(
+        List[Resistance],
+        default=[],
+        stringifier=_abstract_list_attr("Neutral"),
+        deserializer=lambda data: [Resistance.from_dict(r) for r in data],
+        serializer=lambda data: [r.to_dict() for r in data],
+    )
+    ac_value = _PassiveEffect(int, stringifier=_abstract_str_attr("AC"))
+    ac_bonus = _PassiveEffect(int, stringifier=_abstract_str_attr("AC Bonus"))
+    max_hp_value = _PassiveEffect(int, stringifier=_abstract_str_attr("Max HP"))
+    max_hp_bonus = _PassiveEffect(int, stringifier=_abstract_str_attr("Max HP Bonus"))
+    save_bonus = _PassiveEffect(str, stringifier=_abstract_str_attr("Save Bonus"))
+    save_adv = _PassiveEffect(
+        Set[str],
+        default=set(),
+        stringifier=_str_save_adv,
+        deserializer=lambda data: set(data),
+        serializer=lambda data: list(data),
+    )
+    save_dis = _PassiveEffect(
+        Set[str],
+        default=set(),
+        stringifier=_str_save_adv,
+        deserializer=lambda data: set(data),
+        serializer=lambda data: list(data),
+    )
+    check_bonus = _PassiveEffect(str, stringifier=_abstract_str_attr("Check Bonus"))
+
+    def __init__(self, **kwargs):
+        for attr in kwargs:
+            if attr not in self.__effect_attrs:
+                raise ValueError(f"{attr!r} is not a valid effect attribute.")
+            setattr(self, attr, kwargs[attr])
 
     @classmethod
     def from_dict(cls, d):
-        # special deserialization logic
-        if "attack_advantage" in d:
-            d["attack_advantage"] = AdvantageType(d["attack_advantage"])
-        if "resistances" in d:
-            d["resistances"] = [Resistance.from_dict(r) for r in d["resistances"]]
-        if "immunities" in d:
-            d["immunities"] = [Resistance.from_dict(r) for r in d["immunities"]]
-        if "vulnerabilities" in d:
-            d["vulnerabilities"] = [Resistance.from_dict(r) for r in d["vulnerabilities"]]
-        if "ignored_resistances" in d:
-            d["ignored_resistances"] = [Resistance.from_dict(r) for r in d["ignored_resistances"]]
-        if "save_adv" in d:
-            d["save_adv"] = set(d["save_adv"])
-        if "save_dis" in d:
-            d["save_dis"] = set(d["save_dis"])
-        return cls(**d)
+        for attr in d:
+            if attr not in cls.__effect_attrs:
+                raise ValueError(f"{attr!r} is not a valid effect attribute.")
+        return cls(**{k: cls.__effect_deserializers[k](v) for k, v in d.items()})
 
     def to_dict(self):
         out = {}
-        for attr in self.__slots__:
+        for attr in self.__effect_attrs:
             if value := getattr(self, attr):
-                out[attr] = value
-
-        # special serialization logic
-        if "attack_advantage" in out:
-            out["attack_advantage"] = self.attack_advantage.value
-        if "resistances" in out:
-            out["resistances"] = [r.to_dict() for r in self.resistances]
-        if "immunities" in out:
-            out["immunities"] = [r.to_dict() for r in self.immunities]
-        if "vulnerabilities" in out:
-            out["vulnerabilities"] = [r.to_dict() for r in self.vulnerabilities]
-        if "ignored_resistances" in out:
-            out["ignored_resistances"] = [r.to_dict() for r in self.ignored_resistances]
-        if "save_adv" in out:
-            out["save_adv"] = list(out["save_adv"])
-        if "save_dis" in out:
-            out["save_dis"] = list(out["save_dis"])
-
+                out[attr] = self.__effect_serializers[attr](value)
         return out
 
     def __str__(self):
         values = []
-        for attr in self.__slots__:
+        for attr in self.__effect_attrs:
             value = self._str_attr(attr)
             if value:
                 values.append(value)
@@ -161,7 +208,7 @@ class InitPassiveEffect:
 
     def __bool__(self):
         """True if there are any passive effects."""
-        return any(getattr(self, attr) for attr in self.__slots__)
+        return any(getattr(self, attr) for attr in self.__effect_attrs)
 
     # ==== construction ====
     @classmethod
@@ -169,7 +216,7 @@ class InitPassiveEffect:
         ac_value, ac_bonus = resolve_value_or_bonus(args.get("ac"))
         max_hp_value, max_hp_bonus = resolve_value_or_bonus(args.get("maxhp"))
         return cls(
-            attack_advantage=args.adv(ea=True),
+            attack_advantage=AdvantageType(args.adv(ea=True)),
             to_hit_bonus=args.join("b", "+"),
             damage_bonus=args.join("d", "+"),
             magical_damage="magical" in args,
@@ -199,46 +246,8 @@ class InitPassiveEffect:
         if not value:
             return
 
-        value_handler = getattr(self, f"_str_{attr}")
+        value_handler = self.__effect_stringifiers[attr]
         return value_handler(value)
-
-    _str_to_hit_bonus = _abstract_str_attr("Attack Bonus")
-    _str_damage_bonus = _abstract_str_attr("Damage Bonus")
-    _str_magical_damage = _abstract_bool_attr("Magical Damage")
-    _str_silvered_damage = _abstract_bool_attr("Silvered Damage")
-    _str_resistances = _abstract_list_attr("Resistance")
-    _str_immunities = _abstract_list_attr("Immunity")
-    _str_vulnerabilities = _abstract_list_attr("Vulnerability")
-    _str_ignored_resistances = _abstract_list_attr("Neutral")
-    _str_ac_value = _abstract_str_attr("AC")
-    _str_ac_bonus = _abstract_str_attr("AC Bonus")
-    _str_max_hp_value = _abstract_str_attr("Max HP")
-    _str_max_hp_bonus = _abstract_str_attr("Max HP Bonus")
-    _str_save_bonus = _abstract_str_attr("Save Bonus")
-    _str_check_bonus = _abstract_str_attr("Check Bonus")
-
-    @staticmethod
-    def _str_attack_advantage(value: AdvantageType):
-        if value == AdvantageType.ADV:
-            return "Attack Advantage"
-        elif value == AdvantageType.DIS:
-            return "Attack Disadvantage"
-        elif value == AdvantageType.ELVEN:
-            return "Attack Advantage: Elven Accuracy"
-
-    @staticmethod
-    def _str_save_adv(value: Set[str]):
-        if value.issuperset(STAT_ABBREVIATIONS):
-            return "Save Advantage: All"
-        saves = ", ".join(verbose_stat(s) for s in value)
-        return f"Save Advantage: {saves}"
-
-    @staticmethod
-    def _str_save_dis(value: Set[str]):
-        if value.issuperset(STAT_ABBREVIATIONS):
-            return "Save Disadvantage: All"
-        saves = ", ".join(verbose_stat(s) for s in value)
-        return f"Save Disadvantage: {saves}"
 
 
 def resolve_value_or_bonus(values: List[str]) -> Tuple[Optional[int], Optional[int]]:

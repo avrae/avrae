90a2f7705649d26d988428f0a7b297ff9d24bdae
Andrew Zhu <me@andrew-zhu.com>
2022-06-06 12:49:49 -0700

    refactor: ensure automation results are dictable

--- cogs5e/models/automation/effects/castspell.py
+++ cogs5e/models/automation/effects/castspell.py
@@ -50,11 +50,12 @@ class CastSpell(Effect):
         cast_level = self.level if self.level is not None else spell.level
         if not spell.level <= cast_level <= 9:
             autoctx.meta_queue(f"**Error**: Unable to cast {spell.name} at level {cast_level} (invalid level).")
-            return CastSpellResult(success=False, spell=spell)
+            return CastSpellResult(success=False, spell_id=self.id)
         if autoctx.is_spell:
             autoctx.meta_queue(f"**Error**: Unable to cast another spell inside a spell.")
-            return CastSpellResult(success=False, spell=spell)
+            return CastSpellResult(success=False, spell_id=self.id)
 
+        dc_override = ab_override = spell_override = None
         if spell.automation and spell.automation.effects:
             # save old autoctx values
             old_ab_override = autoctx.ab_override
@@ -64,11 +65,11 @@ class CastSpell(Effect):
 
             # run the spell using the given values
             if self.attack_bonus is not None:
-                autoctx.ab_override = autoctx.parse_intexpression(self.attack_bonus)
+                ab_override = autoctx.ab_override = autoctx.parse_intexpression(self.attack_bonus)
             if self.dc is not None:
-                autoctx.dc_override = autoctx.parse_intexpression(self.dc)
+                dc_override = autoctx.dc_override = autoctx.parse_intexpression(self.dc)
             if self.casting_mod is not None:
-                autoctx.evaluator.builtins["spell"] = autoctx.parse_intexpression(self.casting_mod)
+                spell_override = autoctx.evaluator.builtins["spell"] = autoctx.parse_intexpression(self.casting_mod)
             if self.level is not None:
                 autoctx.spell_level_override = self.level
             autoctx.spell = spell
@@ -95,7 +96,15 @@ class CastSpell(Effect):
                 autoctx.queue(smart_trim(spell.higherlevels))
                 autoctx.push_embed_field(title="At Higher Levels")
 
-        return CastSpellResult(success=True, spell=spell, children=results)
+        return CastSpellResult(
+            success=True,
+            spell_id=self.id,
+            level_override=self.level,
+            dc_override=dc_override,
+            attack_bonus_override=ab_override,
+            casting_mod_override=spell_override,
+            children=results,
+        )
 
     def build_str(self, caster, evaluator):
         super().build_str(caster, evaluator)
--- cogs5e/models/automation/results.py
+++ cogs5e/models/automation/results.py
@@ -1,4 +1,5 @@
 import abc
+import dataclasses
 from dataclasses import dataclass, field
 from typing import Iterable, List, Optional, TYPE_CHECKING
 
@@ -6,7 +7,7 @@ import d20
 
 if TYPE_CHECKING:
     from cogs5e.initiative import InitiativeEffect
-    from gamedata import Spell
+    from .effects.roll import RollEffectMetaVar
 
 __all__ = (
     "EffectResult",
@@ -28,6 +29,7 @@ __all__ = (
 )
 
 
+@dataclass(frozen=True)
 class EffectResult(abc.ABC):
     """
     Base class for the result of an automation effect.
@@ -43,6 +45,9 @@ class EffectResult(abc.ABC):
 
     def __iter__(self):
         yield from self.get_children()
+
+    def to_dict(self):
+        return dataclasses.asdict(self, dict_factory=lambda e: e.to_dict())
 
 
 @dataclass(frozen=True)
@@ -89,15 +94,36 @@ class AttackResult(EffectResult):
     did_crit: bool
     children: List[EffectResult]
 
+    def to_dict(self):
+        return {
+            "attack_bonus": self.attack_bonus,
+            "ac": self.ac,
+            "to_hit_roll": self.to_hit_roll and roll_result_to_dict(self.to_hit_roll),
+            "adv": self.adv,
+            "did_hit": self.did_hit,
+            "did_crit": self.did_crit,
+            "children": [child.to_dict() for child in self.children],
+        }
+
 
 @dataclass(frozen=True)
 class SaveResult(EffectResult):
     dc: int
-    ability: str
+    ability: str  # element of utils.constants.SAVE_NAMES, e.g. strengthSave
     save_roll: Optional[d20.RollResult]  # None if target is simple or automatic fail/pass
     adv: int
     did_save: bool
     children: List[EffectResult]
+
+    def to_dict(self):
+        return {
+            "dc": self.dc,
+            "ability": self.ability,
+            "save_roll": self.save_roll and roll_result_to_dict(self.save_roll),
+            "adv": self.adv,
+            "did_save": self.did_save,
+            "children": [child.to_dict() for child in self.children],
+        }
 
 
 @dataclass(frozen=True)
@@ -109,11 +135,17 @@ class DamageResult(EffectResult):
     def get_damage(self) -> int:
         return self.damage
 
+    def to_dict(self):
+        return {"damage": self.damage, "damage_roll": roll_result_to_dict(self.damage_roll), "in_crit": self.in_crit}
+
 
 @dataclass(frozen=True)
 class TempHPResult(EffectResult):
     amount: int
     amount_roll: d20.RollResult
+
+    def to_dict(self):
+        return {"amount": self.amount, "amount_roll": roll_result_to_dict(self.amount_roll)}
 
 
 @dataclass(frozen=True)
@@ -121,19 +153,36 @@ class IEffectResult(EffectResult):
     effect: "InitiativeEffect"
     conc_conflict: List["InitiativeEffect"]
 
+    def to_dict(self):
+        return {"effect": self.effect.to_dict(), "conc_conflict": [e.to_dict() for e in self.conc_conflict]}
+
 
 @dataclass(frozen=True)
 class RemoveIEffectResult(EffectResult):
     removed_effect: "InitiativeEffect"
     removed_parent: Optional["InitiativeEffect"]
 
+    def to_dict(self):
+        return {
+            "removed_effect": self.removed_effect.to_dict(),
+            "removed_parent": self.removed_parent and self.removed_parent.to_dict(),
+        }
+
 
 @dataclass(frozen=True)
 class RollResult(EffectResult):
     result: int
     roll: d20.RollResult
-    simplified_expr: d20.Expression
+    simplified_metavar: "RollEffectMetaVar"
     hidden: bool
+
+    def to_dict(self):
+        return {
+            "result": self.result,
+            "roll": roll_result_to_dict(self.roll),
+            "simplified_metavar": str(self.simplified_metavar),
+            "hidden": self.hidden,
+        }
 
 
 @dataclass(frozen=True)
@@ -167,16 +216,42 @@ class UseCounterResult(EffectResult):
 @dataclass(frozen=True)
 class CastSpellResult(EffectResult):
     success: bool
-    spell: Optional["Spell"] = None
+    spell_id: Optional[int] = None
+    level_override: Optional[int] = None
+    dc_override: Optional[int] = None
+    attack_bonus_override: Optional[int] = None
+    casting_mod_override: Optional[int] = None
     children: List[EffectResult] = field(default_factory=list)
 
 
 @dataclass(frozen=True)
 class CheckResult(EffectResult):
+    skill_key: str
     skill_name: str
     check_roll: Optional[d20.RollResult]  # None if target is simple or automatic fail/pass
     dc: Optional[int]
     contest_roll: Optional[d20.RollResult]
+    contest_skill_key: Optional[str]
     contest_skill_name: Optional[str]
+    contest_did_tie: bool
     did_succeed: Optional[bool]
     children: List[EffectResult]
+
+    def to_dict(self):
+        return {
+            "skill_key": self.skill_key,
+            "skill_name": self.skill_name,
+            "check_roll": self.check_roll and roll_result_to_dict(self.check_roll),
+            "dc": self.dc,
+            "contest_roll": self.contest_roll and roll_result_to_dict(self.contest_roll),
+            "contest_skill_key": self.contest_skill_key,
+            "contest_skill_name": self.contest_skill_name,
+            "contest_did_tie": self.contest_did_tie,
+            "did_succeed": self.did_succeed,
+            "children": [c.to_dict() for c in self.children],
+        }
+
+
+# ==== helpers ====
+def roll_result_to_dict(roll_result: d20.RollResult) -> dict:
+    return {"total": roll_result.total, "result": roll_result.result}

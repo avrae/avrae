24348980df73bf6b07c945776e135370e04cf3b1
Andrew Zhu <me@andrew-zhu.com>
2022-06-24 13:26:33 -0700

    build: simulate all buttons and actions from ieffects

--- tests/gamedata/simulation_utils.py
+++ tests/gamedata/simulation_utils.py
@@ -1,24 +1,87 @@
 import json
+import logging
 
 import disnake
 
 from cogs5e.initiative.combatant import Combatant, MonsterCombatant, PlayerCombatant
+from cogs5e.models.automation import EffectResult, IEffectResult
 from cogs5e.models.sheet.base import Skill
 from cogs5e.models.sheet.resistance import Resistances
-from gamedata import Monster, compendium
+from gamedata import Monster, Spell, compendium
 from tests.discord_mock_data import DEFAULT_USER_ID
 from tests.utils import ContextBotProxy, requires_data
 from utils.argparser import argparse
 
+log = logging.getLogger(__name__)
 
-async def run_automation(automation, avrae, combat=None, **kwargs):
-    """Wrapper function to help supply some defaults"""
 
-    default_kwargs = dict(ctx=ContextBotProxy(avrae), embed=disnake.Embed(), args=argparse(""), combat=combat)
+async def run_automation(automation, avrae, combat=None, targets=None, **kwargs):
+    """
+    Runs the given automation and runs basic assertions on its results.
+
+    If *combat* is given and the automation returns ieffects granting attacks or buttons, those will be tested
+    recursively.
+    """
+    if targets is None:
+        targets = []
+
+    default_kwargs = dict(
+        ctx=ContextBotProxy(avrae), embed=disnake.Embed(), args=argparse(""), combat=combat, targets=targets
+    )
     result = await automation.run(**{**default_kwargs, **kwargs})
     result_dict = result.to_dict()  # AutomationResult must be dict-serializable
     assert json.dumps(result_dict)  # and JSON-serializable
-    return result
+
+    log.info("Completed root automation")
+
+    async def _recurse_results(result: EffectResult):
+        """Recurse over an automation result, and run granted buttons and attacks"""
+        for node in result:
+            if isinstance(node, IEffectResult) and node.effect.combatant is not None:
+                log.info(f"Found IEffect with combatant: {node.effect.name} on {node.effect.combatant.name}")
+                for attack_interaction in node.effect.attacks:
+                    # simulate running the attack against the same targets
+                    attack = attack_interaction.attack
+                    log.info(f"Simulating granted action: {attack.name}")
+                    await run_automation(
+                        attack.automation,
+                        avrae,
+                        combat=combat,
+                        caster=node.effect.combatant,
+                        targets=targets,
+                        **attack.__run_automation_kwargs__,
+                    )
+                    log.info(f"Finished simulating granted action: {attack.name}")
+                for button in node.effect.buttons:
+                    # simulate clicking the button
+                    # this is pretty much just copied from cogs5e.initiative.buttons
+                    log.info(f"Simulating granted button: {button.label}")
+                    if button.granting_spell_id is not None:
+                        spell = compendium.lookup_entity(Spell.entity_type, button.granting_spell_id)
+                    else:
+                        spell = None
+                    await run_automation(
+                        button.automation,
+                        avrae,
+                        combat=combat,
+                        caster=node.effect.combatant,
+                        targets=[],
+                        ieffect=node.effect,
+                        allow_caster_ieffects=False,
+                        # do not allow things like damage-boosting effects to affect dot ticks
+                        ab_override=button.override_default_attack_bonus,
+                        dc_override=button.override_default_dc,
+                        spell_override=button.override_default_casting_mod,
+                        spell=spell,
+                        spell_level_override=button.granting_spell_cast_level,
+                        from_button=True,
+                    )
+                    log.info(f"Finished simulating granted button: {button.label}")
+                log.info(f"Finished IEffect on combatant: {node.effect.name} on {node.effect.combatant.name}")
+            await _recurse_results(node)
+
+    if combat is not None:
+        await _recurse_results(result)
 
 
 def create_basic_combatant(combat, avrae):

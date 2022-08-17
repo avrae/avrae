8b1030e5fa052a3778201719b912d865452cdf35
LazyDope <mydj555@gmail.com>
2022-08-16 21:13:43 -0400

    blackify

--- aliasing/evaluators.py
+++ aliasing/evaluators.py
@@ -440,7 +440,7 @@ class ScriptingEvaluator(draconic.DraconicInterpreter):
                 yaml.composer.Composer.__init__(self)
                 DraconicConstructor.__init__(self)
                 yaml.resolver.Resolver.__init__(self)
-                self.inf_value = float('inf')
+                self.inf_value = float("inf")
 
         return DraconicLoader
 

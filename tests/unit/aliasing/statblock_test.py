6591844554dbdfa10f99c0519399154e94acd425
Chris A <1drturtle@gmail.com>
2022-11-13 22:55:43 -0500

    feat(aliasing) AliasBaseStats.get

--- tests/unit/aliasing/statblock_test.py
+++ tests/unit/aliasing/statblock_test.py
@@ -1,9 +1,37 @@
-from aliasing.api.statblock import AliasSkill
-from cogs5e.models.sheet.base import Skill
+from aliasing.api.statblock import AliasSkill, AliasBaseStats
+from cogs5e.models.sheet.base import Skill, BaseStats
 
 
 # TODO: AliasStatBlock tests
 # TODO: AliasBaseStats tests
+def test_alias_base_stats():
+    stats = BaseStats.default()
+    alias_stats = AliasBaseStats(stats)
+
+    assert alias_stats.strength == 10
+    assert alias_stats.get("strength") == 10
+    assert alias_stats.get_mod("strength") == 0
+
+    assert alias_stats.dexterity == 10
+    assert alias_stats.get("dexterity") == 10
+    assert alias_stats.get_mod("dexterity") == 0
+
+    assert alias_stats.constitution == 10
+    assert alias_stats.get("constitution") == 10
+    assert alias_stats.get_mod("constitution") == 0
+
+    assert alias_stats.wisdom == 10
+    assert alias_stats.get("wisdom") == 10
+    assert alias_stats.get_mod("wisdom") == 0
+
+    assert alias_stats.intelligence == 10
+    assert alias_stats.get("intelligence") == 10
+    assert alias_stats.get_mod("intelligence") == 0
+
+    assert alias_stats.charisma == 10
+    assert alias_stats.get("charisma") == 10
+    assert alias_stats.get_mod("charisma") == 0
+
 # TODO: AliasLevels tests
 # TODO: AliasAttack tests
 # TODO: AliasSkill tests

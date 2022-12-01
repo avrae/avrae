452af785f262a81158cdf7a251c1fff7c4e33cbd
LazyDope <mydj555@gmail.com>
2022-12-01 02:00:06 -0500

    feat(dcv2): add dice to autoparser

--- cogs5e/models/dicecloud/autoparser.py
+++ cogs5e/models/dicecloud/autoparser.py
@@ -6,8 +6,18 @@ from cogs5e.models.automation import Automation
 from utils.constants import STAT_ABBREVIATIONS
 from utils.functions import chunk_text
 
+# because I like attributes
 Effects = collections.namedtuple("Effects", ["damage", "saves", "save_damage"])
-NO_DICE_COUNT = re.compile(r"(?<!\d)d(?=\d)")
+
+# various regex for annotated strings
+NO_DICE_COUNT = re.compile(r"(?=\s|^)d(?=[\d{])")
+THE_DICE_D = re.compile(r"(?=^|\b)([\d}]+)d([\d{]+)(?=$|\b)")
+IF_TRUE_FALSE = re.compile(r"([^?(]*)\?([^:]*):([^)]*)")
+MAGIC_ANNOSTR_REGEX = re.compile(r"(\(?(([a-ce-zA-Z_]|(?<!\w)\d+(?!d\d+)|d(?!\d+))\w*).*?\)?)($|\s*[+\-*/]?\s*\d*d\d+)")
+SPECIAL_FUNCS = (
+    (re.compile(r"\btrunc\b"), "int"),
+    (re.compile(r"sign\(([^\)]*)\)"), r"((\1) and round((\1)/abs(\1)))"),
+)
 
 log = logging.getLogger(__name__)
 
@@ -25,16 +35,28 @@ class DCV2AutoParser:
     def get_automation(self, prop):
         self.parse(prop)
 
-        auto = []
+        auto = []  # all the automation is in here
+
+        # the insert the action description as the first text
         desc = prop.get("summary", {}).get("value") or prop.get("description", {}).get("value")
         if desc is not None:
             self.text.insert(0, desc)
+
+        # add counters for each resource found
         for resource, amt in self.resources:
             auto.append({"type": "counter", "counter": resource, "amount": str(amt)})
 
+        # add all the rolls to the automation data
+        for name, roll in self.rolls.items():
+            auto.append({"type": "roll", "name": name} | roll)
+
+        # check if we actually need an all target, then create and add effects
         if (damages := self.target_effects.damage) or self.target_effects.saves or self.meta.get("bonus") is not None:
+            # easiest way I could think of to get a reference back
             stack = []
             auto.append({"type": "target", "target": "all", "effects": stack.append([]) or stack[-1]})
+
+            # add attack and damage nodes
             if bonus := self.meta.get("bonus"):
                 stack[-1].append(
                     {"type": "attack", "hit": stack.append([]) or stack[-1], "miss": [], "attackBonus": str(bonus)}
@@ -44,7 +66,7 @@ class DCV2AutoParser:
                     {"type": "damage", "damage": f"{damage['damage']}[{damage['type']}]", "overheal": False}
                 )
 
-            # do save stuff
+            # from parsed data, create saves for target
             for save in self.target_effects.saves:
                 if (stat := save["stat"][:3].lower()) in STAT_ABBREVIATIONS:
                     stack[-1].append(
@@ -56,11 +78,13 @@ class DCV2AutoParser:
                             "dc": save["dc"],
                         }
                     )
+            # tack on all save damage to the saves, not very accurate I suppose
             for damage in self.target_effects.save_damage:
                 stack[-1].append(
                     {"type": "damage", "damage": f"{damage['damage']}[{damage['type']}]", "overheal": False}
                 )
 
+        # same as all targets, but this time for the caster
         if (
             (damages := self.self_effects.damage)
             or self.self_effects.saves
@@ -77,7 +101,6 @@ class DCV2AutoParser:
                     {"type": "damage", "damage": f"{damage['damage']}[{damage['type']}]", "overheal": False}
                 )
 
-            # do save stuff
             for save in self.self_effects.saves:
                 if (stat := save["stat"][:3].lower()) in STAT_ABBREVIATIONS:
                     stack[-1].append(
@@ -93,6 +116,7 @@ class DCV2AutoParser:
                 stack[-1].append(
                     {"type": "damage", "damage": f"{damage['damage']}[{damage['type']}]", "overheal": False}
                 )
+        # add all the text at the end
         for text in self.text:
             auto.extend({"type": "text", "text": chunk} for chunk in chunk_text(text))
 
@@ -105,6 +129,7 @@ class DCV2AutoParser:
         return Automation.from_data(auto)
 
     def parse(self, prop, *, initial=True, save=False):
+        # most types have unique effects under an action
         match prop["type"]:
             case "action" | "spell":
                 if initial:
@@ -144,17 +169,16 @@ class DCV2AutoParser:
                     )
                 self.parse_children(prop["children"], save=True)
             case "damage":
+                # all the checks for what exactly we're doing here
                 magical = "magical" in prop["tags"]
                 healing = prop["damageType"] == "healing"
                 effects = [str(effect["amount"]["value"]).strip() for effect in prop["amount"].get("effects", [])]
                 damage_dice = str(prop["amount"]["value"]) + "".join(
                     effect if effect[0] in "+-" else f"+{effect}" for effect in effects
                 )
-                damage = re.sub(
-                    NO_DICE_COUNT,
-                    "1d",
-                    f"{'-1*(' if healing else ''}{damage_dice}{')' if healing else ''}".lower(),
-                )
+                damage_dice = f"{'-1*(' if healing else ''}{damage_dice}{')' if healing else ''}"
+                # handle all the annotated string stuff, as well as a few funcs
+                damage = self.convert_to_annostr(damage_dice)
                 effects = (
                     self.self_effects if self.meta.get("self") or prop.get("target") == "self" else self.target_effects
                 )
@@ -170,19 +194,32 @@ class DCV2AutoParser:
                     effects.damage.append(damage)
                 self.parse_children(prop["children"], save=save)
             case "buff":
-                pass
+                pass  # maybe someday we can convert these into ieffects
             case "toggle":
+                # since all properties under actions are inactive, we have to check the toggles
                 if prop["condition"]["value"]:
                     self.parse_children(prop["children"], save=save)
             case "branch":
+                # these branch types specifically are banned
                 if prop["branchType"] in ("index", "random"):
                     return
                 self.parse_children(prop["children"], save=save)
             case "note":
+                # we only use the summary here, since it's all DC would display
                 desc = prop.get("summary")
                 if desc is not None:
                     self.text.append(desc)
                 self.parse_children(prop["children"], save=save)
+            # rolls, pretty straight forward
+            case "roll":
+                if name := prop.get("variableName"):
+                    roll = {"dice": prop.get("roll", {}).get("value")}
+                    if roll["dice"]:
+                        roll["dice"] = self.convert_to_annostr(roll["dice"])
+                        if d_name := prop.get("name"):
+                            roll["displayName"] = d_name
+                        self.rolls[name] = roll
+            # everything else does nothing and just runs its children
             case _:
                 self.parse_children(prop["children"], save=save)
 
@@ -190,3 +227,13 @@ class DCV2AutoParser:
         for child_id in children:
             child_prop = self.parser._by_id[child_id]
             self.parse(child_prop, initial=False, save=save)
+
+    @staticmethod
+    def convert_to_annostr(string: str):
+        string = re.sub(IF_TRUE_FALSE, r"(\2) if (\1) else (\3)", string)
+        string = re.sub(MAGIC_ANNOSTR_REGEX, r"{\1}\4", string)
+        string = re.sub(NO_DICE_COUNT, "1d", string)
+        string = re.sub(THE_DICE_D, r"\1d\2", string)
+        for patt, rep in SPECIAL_FUNCS:
+            string = re.sub(patt, rep, string)
+        return string

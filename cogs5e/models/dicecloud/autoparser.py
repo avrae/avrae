38d2bcf516208dc1b7559b87baf2b37c7a37b81e
Byron Pettigrew <mydj555@gmail.com>
2022-12-04 12:01:47 -0500

    fix(dcv2): regex is hard

--- cogs5e/models/dicecloud/autoparser.py
+++ cogs5e/models/dicecloud/autoparser.py
@@ -14,7 +14,7 @@ Effects = collections.namedtuple("Effects", ["damage", "saves", "save_damage"])
 NO_DICE_COUNT = re.compile(r"(?=\s|^)d(?=[\d{])")
 THE_DICE_D = re.compile(r"(?=^|\b)(?:[\d}]+)d(?:[\d{]+)(?=$|\b)")
 IF_TRUE_FALSE = re.compile(r"([^?(]*)\?([^:]*):([^)]*)")
-MAGIC_ANNOSTR_REGEX = re.compile(r"(\(?(?:(?:[a-ce-zA-Z_]|(?<!\w)\d+(?!\d*d\d+)|d(?!\d+))\w*).*?\)?)($|\s*[+\-*/]?\s*\d*d\d+)")
+MAGIC_ANNOSTR_REGEX = re.compile(r"(\s*)(\(?\s*?(?:(?<!\w)\d+(?!\d*d\d+(?=\s|$|\b)).*?)?(?:(?:[a-ce-zA-Z_]|d(?!\d+(?=\s|$|\b)))\w*).*?\)?)(?=$|\s*[+\-*/]?[+\-*/(\d\s]*(?:\d*d\d+))")
 SPECIAL_FUNCS = (
     (re.compile(r"\btrunc\b"), "int"),
     (re.compile(r"sign\(([^\)]*)\)"), r"((\1) and round((\1)/abs(\1)))"),
@@ -237,7 +237,7 @@ class DCV2AutoParser:
     @staticmethod
     def convert_to_annostr(string: str):
         string = re.sub(IF_TRUE_FALSE, r"(\2) if (\1) else (\3)", string, re.IGNORECASE)
-        string = re.sub(MAGIC_ANNOSTR_REGEX, r"{\1}\2", string, re.IGNORECASE)
+        string = re.sub(MAGIC_ANNOSTR_REGEX, r"\1{\2}", string)
         string = re.sub(NO_DICE_COUNT, "1d", string, re.IGNORECASE)
         string = re.sub(THE_DICE_D, "d", string, re.IGNORECASE)
         for patt, rep in SPECIAL_FUNCS:

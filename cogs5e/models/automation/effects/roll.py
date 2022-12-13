521b15942efc40c082845283ea021a979278d17b
LazyDope <mydj555@gmail.com>
2022-12-13 17:59:11 -0500

    fix(auto): comp funcs should return bool

--- cogs5e/models/automation/effects/roll.py
+++ cogs5e/models/automation/effects/roll.py
@@ -124,22 +124,22 @@ class RollEffectMetaVar:
         return bool(self._total)
 
     def __eq__(self, other):
-        return self._lbin_op(other, "==")
+        return self._total == other
 
     def __lt__(self, other):
-        return self._lbin_op(other, "<")
+        return self._total < other
 
     def __le__(self, other):
-        return self._lbin_op(other, "<=")
+        return self._total <= other
 
     def __ne__(self, other):
-        return self._lbin_op(other, "!=")
+        return self._total != other
 
     def __gt__(self, other):
-        return self._lbin_op(other, ">")
+        return self._total > other
 
     def __ge__(self, other):
-        return self._lbin_op(other, ">=")
+        return self._total >= other
 
     def __floor__(self):
         return math.floor(self._total)

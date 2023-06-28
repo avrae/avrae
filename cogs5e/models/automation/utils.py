9532b222aba83641b8a84c9b91287ec497d9aff3
Joseph Keen <icrona@gmail.com>
2023-06-28 09:53:12 -0600

    Update utils.py

--- cogs5e/models/automation/utils.py
+++ cogs5e/models/automation/utils.py
@@ -28,7 +28,7 @@ def upcast_scaled_dice(effect, autoctx, dice_ast):
             level_dice = 3
         else:
             level_dice = 4
-        level_dice = autoctx.args.last("cantripdice",default=level_dice,type_=int)
+        level_dice = autoctx.args.last("cantripdice", default=level_dice, type_=int)
         def mapper(node):
             if isinstance(node, d20.ast.Dice):
                 node.num = level_dice

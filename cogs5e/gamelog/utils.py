83d970600e066843272ec68302634645838a1f44
Sean Stoves <sean.stoves@wizards.com>
2024-04-26 01:38:27 -0400

    Updating for some additional error handling.

--- cogs5e/gamelog/utils.py
+++ cogs5e/gamelog/utils.py
@@ -3,6 +3,7 @@ import logging
 
 from ddb.gamelog import GameLogEventContext
 from ddb.gamelog.errors import IgnoreEvent
+import ldclient
 
 
 def feature_flag(flag_name, default=False):
@@ -25,13 +26,13 @@ def feature_flag(flag_name, default=False):
             # custom attributes/username/etc
             # note: this means that feature flag targeting can only be controlled by global or individual user id
             # but still, better than nothing
-            user = await gctx.bot.ddb.get_ddb_user(gctx, gctx.event.user_id)
+            user = gctx.event.user_id
 
             if not user:
                 raise IgnoreEvent(f"User {gctx.event.user_id} has not connected their account")
-            flag_on = await gctx.bot.ldclient.variation_for_ddb_user(flag_name, user, False, discord_id=gctx.get_discord_user())
+            flag_on = await gctx.bot.ldclient.variation(flag_name, ldclient.Context.create(user), False)
             if not flag_on:
-                raise IgnoreEvent(f"Feature flag {flag_name!r} is disabled for user {user.username}")
+                raise IgnoreEvent(f"Feature flag {flag_name!r} is disabled for user {user}")
             return await inner(self, gctx, *args, **kwargs)
 
         return wrapped

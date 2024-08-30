4c53b9d6061f2e9d4f44dd92e57947d6f1c5105f
Sean Stoves <sean@seanstoves.com>
2024-08-30 14:42:43 -0400

    Black Formatting

--- cogsmisc/adminUtils.py
+++ cogsmisc/adminUtils.py
@@ -365,7 +365,6 @@ class AdminUtils(commands.Cog):
         except Exception as e:
             await ctx.send(f"Error registering slash commands: {e}")
 
-
     # ---- workshop ----
     @admin.group(name="workshop", invoke_without_command=True)
     @checks.is_owner()

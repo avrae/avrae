91001284b4613c2bfc84b4507d474ab5eb1e6465
Corvux <bryce.hadsell@gmail.com>
2025-03-06 06:23:26 -0600

    Allow `list_display_override` to be an annotated string

--- cogs5e/models/sheet/attack.py
+++ cogs5e/models/sheet/attack.py
@@ -142,7 +142,7 @@ class Attack:
         )
 
     def build_str(self, caster):
-        return f"**{self.name}**: {self.list_display_override or self.automation.build_str(caster)}"
+        return f"**{self.name}**: {caster.evaluate_annostr(self.list_display_override) if self.list_display_override else self.automation.build_str(caster)}"
 
     def __str__(self):
         return f"**{self.name}**: {str(self.automation)}"
@@ -210,20 +210,22 @@ class AttackList:
     @property
     def other_attacks(self):
         """Returns an AttackList of attacks that do not fall into the other action categories."""
-        return AttackList([
-            a
-            for a in self.attacks
-            if a.activation_type
-            not in (
-                enums.ActivationType.ACTION,
-                enums.ActivationType.BONUS_ACTION,
-                enums.ActivationType.REACTION,
-                enums.ActivationType.LEGENDARY,
-                enums.ActivationType.MYTHIC,
-                enums.ActivationType.LAIR,
-                None,
-            )
-        ])
+        return AttackList(
+            [
+                a
+                for a in self.attacks
+                if a.activation_type
+                not in (
+                    enums.ActivationType.ACTION,
+                    enums.ActivationType.BONUS_ACTION,
+                    enums.ActivationType.REACTION,
+                    enums.ActivationType.LEGENDARY,
+                    enums.ActivationType.MYTHIC,
+                    enums.ActivationType.LAIR,
+                    None,
+                )
+            ]
+        )
 
     # list compat
     def append(self, attack):

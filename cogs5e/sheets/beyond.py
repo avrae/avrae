d4a40a767ad513c298904b1464100bd19a813e68
Sean Stoves <sean.stoves@wizards.com>
2025-03-19 21:55:44 -0400

    Improve spell selection logic by considering SAB and MOD values
    Previously, the logic only compared DC values when selecting spells. This update extends the comparison to include SAB and MOD values, ensuring better prioritization of spell information. It enhances accuracy in determining the most relevant spell details.

--- cogs5e/sheets/beyond.py
+++ cogs5e/sheets/beyond.py
@@ -293,6 +293,10 @@ class BeyondSheetParser(SheetLoaderABC):
                     if spells[result.name].prepared:
                         if result.dc and spell_info.dc:
                             spells[result.name] = max(spell_info, spells[result.name], key=lambda x: x.dc)
+                        if result.sab and spell_info.sab:
+                            spells[result.name] = max(spell_info, spells[result.name], key=lambda x: x.sab)
+                        if result.mod and spell_info.mod:
+                            spells[result.name] = max(spell_info, spells[result.name], key=lambda x: x.mod)
 
                     if not spells[result.name].prepared:
                         spells[result.name] = spell_info

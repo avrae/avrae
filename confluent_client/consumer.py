428aaec619028dfcc1d32b88ee03450f5498200c
Kevin Romero <kevkde@gmail.com>
2025-05-14 02:12:13 -0600

    Blackify

--- confluent_client/consumer.py
+++ confluent_client/consumer.py
@@ -1,9 +1,10 @@
 from confluent_kafka import Consumer
 
+
 class KafkaConsumer:
-    '''
+    """
     Kafka consumer class to consume messages from a Kafka topic using Confluent Kafka Python client.
-    
+
     Args:
         config (dict): Kafka consumer configuration.
         config['bootstrap.servers'] (str): Comma-separated list of Kafka brokers.
@@ -13,20 +14,21 @@ class KafkaConsumer:
         config['sasl.mechanisms'] (str): SASL mechanism. Default to PLAIN.
         config['group.id'] (str): Consumer group ID.
         config['auto.offset.reset'] (str): Offset reset policy. Default to earliest.
-        
+
     Attributes:
         consumer (Consumer): Confluent Kafka consumer object.
-        
+
     Methods:
         consume: Consume messages from a Kafka topic.
-        
-    '''
+
+    """
+
     def __init__(self, config):
         self.consumer = Consumer(config)
-        
+
     def consume(self, topic):
         self.consumer.subscribe([topic])
-        
+
         try:
             while True:
                 msg = self.consumer.poll(1.0)
@@ -36,8 +38,11 @@ class KafkaConsumer:
                 if msg.error():
                     print("Consumer error: {}".format(msg.error()))
                     continue
-                print('Consumed event from topic {topic}: key = {key:12} value = {value:12}'.format(
-                    topic=msg.topic(), key=msg.key().decode('utf-8'), value=msg.value().decode('utf-8')))
+                print(
+                    "Consumed event from topic {topic}: key = {key:12} value = {value:12}".format(
+                        topic=msg.topic(), key=msg.key().decode("utf-8"), value=msg.value().decode("utf-8")
+                    )
+                )
                 self.consumer.commit()
         except KeyboardInterrupt:
             pass
--- confluent_client/producer.py
+++ confluent_client/producer.py
@@ -6,32 +6,31 @@ import json
 
 log = logging.getLogger(__name__)
 
+
 class KafkaProducer:
-    '''
+    """
     Kafka producer class to produce messages to a Kafka topic using Confluent Kafka Python client.
-        
+
     Attributes:
         producer (Producer): Confluent Kafka producer object.
-        
+
     Methods:
         delivery_callback: Callback function to handle message delivery status.
         produce: Produce a message to a Kafka topic.
-        
-    '''
-    
+
+    """
+
     config = {
-        'bootstrap.servers': bot_config.KAFKA_BOOTSTRAP_SERVER,
-        'sasl.username': bot_config.KAFKA_API_KEY,
-        'sasl.password': bot_config.KAFKA_API_SECRET,
-        'security.protocol': bot_config.KAFKA_SECURITY_PROTOCOL,
-        'sasl.mechanisms': bot_config.KAFKA_SASL_MECHANISM,
-        'acks': bot_config.KAFKA_ACKS
+        "bootstrap.servers": bot_config.KAFKA_BOOTSTRAP_SERVER,
+        "sasl.username": bot_config.KAFKA_API_KEY,
+        "sasl.password": bot_config.KAFKA_API_SECRET,
+        "security.protocol": bot_config.KAFKA_SECURITY_PROTOCOL,
+        "sasl.mechanisms": bot_config.KAFKA_SASL_MECHANISM,
+        "acks": bot_config.KAFKA_ACKS,
     }
-    
+
     def __init__(self, config):
-        if not config['bootstrap.servers'] \
-        or not config['sasl.username'] \
-        or not config['sasl.password']:
+        if not config["bootstrap.servers"] or not config["sasl.username"] or not config["sasl.password"]:
             self.is_ready = False
             log.warning("Kafka not initialized. Missing configuration.")
             return
@@ -39,28 +38,31 @@ class KafkaProducer:
         self.is_ready = True
         self.producer = Producer(config)
         self.topic = "dnddev_avraebot" if bot_config.TESTING else "dndprod_avraebot"
-        
+
     def delivery_callback(self, err, msg):
         if err:
-            log.error('ERROR: Message failed delivery: {}'.format(err))
+            log.error("ERROR: Message failed delivery: {}".format(err))
         else:
-            log.info("Produced event to topic {topic}: key = {key:12} value = {value:12}".format(
-                topic=msg.topic(), key=msg.key().decode('utf-8'), value=msg.value().decode('utf-8')))
-            
+            log.info(
+                "Produced event to topic {topic}: key = {key:12} value = {value:12}".format(
+                    topic=msg.topic(), key=msg.key().decode("utf-8"), value=msg.value().decode("utf-8")
+                )
+            )
+
     def produce(self, ctx):
-        '''
+        """
         Produce a message to a Kafka topic.
-        
+
         Args:
             ctx (Context): Context object containing message information.
-        '''
+        """
         if not self.is_ready:
             log.warning("Kafka not initialized. Missing configuration.")
             return
         if self.topic is None:
             log.warning("Kafka topic not specified.")
             return
-        
+
         avrae_command = {
             "EVENT_TIME": time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime()),
             "PLATFORM": "discord",
@@ -71,16 +73,16 @@ class KafkaProducer:
             "DISCORD_SERVER_ID": ctx.message.guild.id,
             "COUNTRY_CODE": None,
             "IP_ADDRESS": None,
-            "ACTION_DESCRIPTORS": [
-                {
-                    "COMMAND_ID": ctx.command.qualified_name,
-                    "COMMAND_CATEGORY": ctx.command.cog_name,
-                    "SUBCOMMAND_ID": None,
-                    "ARGS": ctx.message.content.split(" ")[1:],
-                }
-            ],
+            "ACTION_DESCRIPTORS": [{
+                "COMMAND_ID": ctx.command.qualified_name,
+                "COMMAND_CATEGORY": ctx.command.cog_name,
+                "SUBCOMMAND_ID": None,
+                "ARGS": ctx.message.content.split(" ")[1:],
+            }],
         }
-        
-        self.producer.produce(self.topic, json.dumps(avrae_command), str(ctx.message.id), callback=self.delivery_callback)
+
+        self.producer.produce(
+            self.topic, json.dumps(avrae_command), str(ctx.message.id), callback=self.delivery_callback
+        )
         self.producer.poll(10000)
         self.producer.flush()

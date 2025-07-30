22a5642093673e2c28e2c6051a2c5dcb8b7f44d0
vaguely-happy <nichughesmail@gmail.com>
2025-07-30 21:26:04 +0100

    fix(AVR-1069): resolve circular import

--- cogs5e/utils/checkutils.py
+++ cogs5e/utils/checkutils.py
@@ -4,6 +4,7 @@ from typing import Any
 from d20 import roll
 
 import cogs5e.initiative as init
+from cogs5e.initiative.utils import ieffect_handler
 from cogs5e.models import embeds
 from cogs5e.models.errors import InvalidArgument
 from cogs5e.models.sheet.base import Skill
@@ -62,25 +63,7 @@ def run_check(skill_key, caster, args, embed):
 
     # ieffect handling
     if isinstance(caster, init.Combatant):
-        combat_context: dict[str, Any] = {}
-
-        # -cb
-        combat_context["b"] = caster.active_effects(mapper=lambda effect: effect.effects.check_bonus, default=[])
-
-        # -cadv/cdis
-        cadv_effects = caster.active_effects(
-            mapper=lambda effect: effect.effects.check_adv, reducer=lambda checks: set().union(*checks), default=set()
-        )
-        cdis_effects = caster.active_effects(
-            mapper=lambda effect: effect.effects.check_dis, reducer=lambda checks: set().union(*checks), default=set()
-        )
-        if skill_key in cadv_effects or base_ability_key in cadv_effects:
-            combat_context["adv"] = ["True"]
-        if skill_key in cdis_effects or base_ability_key in cdis_effects:
-            combat_context["dis"] = ["True"]
-
-        args.add_context("combat", combat_context)
-        args.set_context("combat")
+        args = ieffect_handler(caster, args, skill_key)
 
     result = _run_common(skill, args, embed, mod_override=mod)
     return CheckResult(rolls=result.rolls, skill=skill, skill_name=skill_name, skill_roll_result=result)
